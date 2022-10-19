14ea22ffa6c9d7a267c6ab72ead9fbae5ac68e6b
Serge Kovbasiuk <isserge@protonmail.com>
2022-10-19 15:35:31 +0300

    change generated files folder

--- codegen.ts
+++ codegen.ts
@@ -3,11 +3,12 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
+  // TODO: don't forget to replace with https endpoint
   schema: "http://164.92.238.249:4444/graphql",
   documents: "./src/components/**/*.{ts,tsx}",
   generates: {
-    "src/generated/graphql.tsx": {
-      preset: "client",
+    './src/gql/': {
+      preset: 'client',
       plugins: []
     }
   }
