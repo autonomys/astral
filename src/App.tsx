df7211672b7a2b8803c991be36bb9c7fde01616d
Serge Kovbasiuk <isserge@protonmail.com>
2022-10-19 16:39:29 +0300

    clean up
    clean up 2
    
    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-
 import { BlockListContainer, ExtrinsicListContainer } from './components';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BlockListContainer />
       <ExtrinsicListContainer />
     </div>
