b5b6eae2ccab734926fa359bfdace8c75fb10b6f
Natacha De la Rosa <delarosa.natacha@gmail.com>
2022-11-01 15:32:40 -0400

    fix(comments): resolver reviewer comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import ExtrinsicListContainer from "ExtrinsicList";
 import { Layout, Container, Footer, Header } from "layout/components";
 // home
 import Home from "Home";
+import { INTERNAL_ROUTES } from "common/routes";
 
 function App() {
   return (
@@ -16,9 +17,15 @@ function App() {
         <Header />
         <Container>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="blocks" element={<BlockListContainer />} />
-            <Route path="extrinsics" element={<ExtrinsicListContainer />} />
+            <Route path={INTERNAL_ROUTES.home} element={<Home />} />
+            <Route
+              path={INTERNAL_ROUTES.blocks}
+              element={<BlockListContainer />}
+            />
+            <Route
+              path={INTERNAL_ROUTES.extrinsics}
+              element={<ExtrinsicListContainer />}
+            />
           </Routes>
         </Container>
         <Footer />
