b68bc792b3ddfdf63a495152dace2673ce38188e
Serge Kovbasiuk <isserge@protonmail.com>
2022-11-30 13:13:43 +0200

    decode log data

--- squid-blockexplorer/src/blocks/getLogs.ts
+++ squid-blockexplorer/src/blocks/getLogs.ts
@@ -1,7 +1,21 @@
 import { SubstrateBlock } from '@subsquid/substrate-processor';
+import { toHex } from '@subsquid/util-internal-hex';
 import { Block, Log } from "../model";
 import { Context } from "../processor";
 import { SystemDigestStorage } from '../types/storage';
+
+function decodeLog(value: null | Uint8Array | Uint8Array[]) {
+  if (!value) return null;
+
+  if (Array.isArray(value)) {
+    return {
+      engine: value[0].toString(),
+      data: toHex(value[1]),
+    };
+  }
+
+  return toHex(value);
+}
 
 export function getLogsFactory(ctx: Context, storageFactory: (ctx: Context, header: SubstrateBlock) => SystemDigestStorage) {
   return async function getLogs(header: SubstrateBlock, block: Block) {
@@ -13,7 +27,7 @@ export function getLogsFactory(ctx: Context, storageFactory: (ctx: Context, head
       kind: log.__kind,
       // uncast to access 'value' prop, which is not present on all DigestItems
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      value: (log as any).value || null,
+      value: decodeLog((log as any).value),
       block,
     }));
   };
