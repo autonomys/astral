d2fc48d16b2f95121fc15b604ffb1351d3c3cb21
Serge Kovbasiuk <isserge@protonmail.com>
2023-01-23 15:43:58 +0100

    unify tabs

--- client/src/Extrinsic/components/ExtrinsicDetailsTab.tsx
+++ client/src/Extrinsic/components/ExtrinsicDetailsTab.tsx
@@ -15,8 +15,14 @@ type Props = {
 const ExtrinsicDetailsTab: FC<Props> = ({ events, isDesktop = false }) => {
   return (
     <Tabs
-      tabStyle={isDesktop ? 'bg-white border border-slate-100 shadow rounded-lg p-4 dark:bg-gradient-to-r dark:from-[#4141B3] dark:via-[#6B5ACF] dark:to-[#896BD2] dark:border-none' : ''}
-      tabTitleStyle={!isDesktop ? 'bg-white rounded-full mb-5 px-5' : ''}
+      tabStyle={
+        isDesktop
+          ? 'bg-white border border-slate-100 shadow rounded-lg p-4 dark:bg-gradient-to-r dark:from-[#4141B3] dark:via-[#6B5ACF] dark:to-[#896BD2] dark:border-none'
+          : ''
+      }
+      tabTitleStyle={!isDesktop ? 'bg-white rounded-full mb-5 px-5 dark:bg-[#1E254E]' : ''}
+      pillStyle={!isDesktop ? 'dark:bg-transparent dark:text-white' : undefined}
+      activePillStyle={!isDesktop ? 'dark:bg-[#DE67E4] dark:text-white' : undefined}
     >
       <Tab title='Events'>
         {isDesktop ? (
--- client/src/Log/components/LogDetailsTab.tsx
+++ client/src/Log/components/LogDetailsTab.tsx
@@ -17,8 +17,14 @@ const LogDetailsTab: FC<Props> = ({ events }) => {
 
   return (
     <Tabs
-      tabStyle={isDesktop ? 'bg-white border border-slate-100 shadow rounded-lg p-4' : ''}
-      tabTitleStyle={!isDesktop ? 'bg-white rounded-full mb-5 px-5' : ''}
+      tabStyle={
+        isDesktop
+          ? 'bg-white border border-slate-100 shadow rounded-lg p-4 dark:bg-gradient-to-r dark:from-[#4141B3] dark:via-[#6B5ACF] dark:to-[#896BD2] dark:border-none'
+          : ''
+      }
+      tabTitleStyle={!isDesktop ? 'bg-white rounded-full mb-5 px-5 dark:bg-[#1E254E]' : ''}
+      pillStyle={!isDesktop ? 'dark:bg-transparent dark:text-white' : undefined}
+      activePillStyle={!isDesktop ? 'dark:bg-[#DE67E4] dark:text-white' : undefined}
     >
       <Tab title='Events'>
         {isDesktop ? (
@@ -51,7 +57,7 @@ const LogDetailsEventCard: FC<EventCardProps> = ({ event }) => {
     <MobileCard
       id='extrinsic-details-event-mobile'
       header={
-        <h3 className='font-medium text-[#241235] text-sm'>{`$${event.block?.height}-${event.pos}`}</h3>
+        <h3 className='font-medium text-[#241235] text-sm dark:text-white'>{`$${event.block?.height}-${event.pos}`}</h3>
       }
       body={body}
     />
