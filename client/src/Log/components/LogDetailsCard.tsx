3e8d5069a2efa29bc94ff2be082f2d80d9948718
Serge Kovbasiuk <isserge@protonmail.com>
2023-02-22 09:58:18 +0400

    update LogDetails Args

--- client/src/Log/components/LogDetailsCard.tsx
+++ client/src/Log/components/LogDetailsCard.tsx
@@ -18,7 +18,9 @@ const LogDetailsCard: FC<Props> = ({ log }) => {
       <div className='flex'>
         <div className='border border-slate-100 bg-white shadow rounded-[20px] mb-4 p-4 sm:p-6 w-full dark:bg-gradient-to-r dark:from-[#4141B3] dark:via-[#6B5ACF] dark:to-[#896BD2] dark:border-none'>
           <div className='flex items-center justify-between mb-10'>
-            <h3 className='font-medium text-sm text-[#241235] md:text-2xl dark:text-white'>Log #{log.id}</h3>
+            <h3 className='font-medium text-sm text-[#241235] md:text-2xl dark:text-white'>
+              Log #{log.id}
+            </h3>
             <div className='bg-[#241235] text-xs font-semibold px-5 py-3 rounded-full block leading-normal text-white'>
               #{log.block.height}
             </div>
@@ -31,7 +33,7 @@ const LogDetailsCard: FC<Props> = ({ log }) => {
                 <StyledListItem title='Data'>-</StyledListItem>
               </List>
             </div>
-            <div className='w-full sm:max-w-xs lg:max-w-md border border-[#F3FBFF] bg-[#F3FBFF] shadow rounded-lg mb-4 p-4 sm:p-6 break-all'>
+            <div className='w-full sm:max-w-xs lg:max-w-md border border-[#F3FBFF] bg-[#F3FBFF] shadow rounded-lg mb-4 p-4 sm:p-6 break-all dark:bg-white/10 dark:border-none'>
               <Arguments args={log.block.events[0].args} />
             </div>
           </div>
