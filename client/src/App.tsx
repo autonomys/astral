82600f54ebc20eea163a0e47b25226c8410f3b72
Natacha De la Rosa <delarosa.natacha@gmail.com>
2023-03-03 16:19:08 -0400

    fix(refresh): case to update correctly the url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import { useLayoutEffect } from 'react'
-import { HashRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom'
+import { useEffect, useLayoutEffect } from 'react'
+import { HashRouter, Routes, Route, useLocation, Navigate, useNavigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { ErrorBoundary } from 'react-error-boundary'
 
@@ -38,58 +38,76 @@ function ScrollToTopWrapper({ children }) {
   return children
 }
 
+// force page scroll to top on route change
+function UpdateURLOnRefresh({ children }) {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const { selectedChain } = useDomains()
+
+  useEffect(() => {
+    if (location.pathname !== selectedChain.urls.page) {
+      navigate(selectedChain.urls.page)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedChain])
+  return children
+}
+
 function App() {
   const { selectedChain } = useDomains()
+
   return (
     <HashRouter>
       <ScrollToTopWrapper>
-        <Layout>
-          {/* TODO: add DomainHeader once we have support for domains */}
-          <Header />
-          <ErrorBoundary
-            fallbackRender={ErrorFallback}
-            onReset={() => window.location.reload()}
-            // TODO: consider adding error monitoring
-            onError={(error) => console.error(error)}
-          >
-            <Container>
-              <HeaderBackground />
-              <Routes>
-                <Route
-                  path={INTERNAL_ROUTES.home}
-                  element={<Navigate to={selectedChain.urls.page} />}
-                />
-                <Route path={':network'}>
-                  <Route index element={<Home />} />
-                  <Route path={INTERNAL_ROUTES.blocks.list}>
-                    <Route index element={<BlockList />} />
-                    <Route element={<Block />} path={INTERNAL_ROUTES.blocks.id.path} />
+        <UpdateURLOnRefresh>
+          <Layout>
+            {/* TODO: add DomainHeader once we have support for domains */}
+            <Header />
+            <ErrorBoundary
+              fallbackRender={ErrorFallback}
+              onReset={() => window.location.reload()}
+              // TODO: consider adding error monitoring
+              onError={(error) => console.error(error)}
+            >
+              <Container>
+                <HeaderBackground />
+                <Routes>
+                  <Route
+                    path={INTERNAL_ROUTES.home}
+                    element={<Navigate to={selectedChain.urls.page} />}
+                  />
+                  <Route path={':network'}>
+                    <Route index element={<Home />} />
+                    <Route path={INTERNAL_ROUTES.blocks.list}>
+                      <Route index element={<BlockList />} />
+                      <Route element={<Block />} path={INTERNAL_ROUTES.blocks.id.path} />
+                    </Route>
+                    <Route path={INTERNAL_ROUTES.extrinsics.list}>
+                      <Route index element={<ExtrinsicList />} />
+                      <Route path={INTERNAL_ROUTES.extrinsics.id.path} element={<Extrinsic />} />
+                    </Route>
+                    <Route path={INTERNAL_ROUTES.accounts.list}>
+                      <Route index element={<AccountList />} />
+                      <Route path={INTERNAL_ROUTES.accounts.id.path} element={<Account />} />
+                    </Route>
+                    <Route path={INTERNAL_ROUTES.events.list}>
+                      <Route index element={<EventList />} />
+                      <Route path={INTERNAL_ROUTES.events.id.path} element={<Event />} />
+                    </Route>
+                    <Route path={INTERNAL_ROUTES.logs.list}>
+                      <Route index element={<LogList />} />
+                      <Route path={INTERNAL_ROUTES.logs.id.path} element={<Log />} />
+                    </Route>
                   </Route>
-                  <Route path={INTERNAL_ROUTES.extrinsics.list}>
-                    <Route index element={<ExtrinsicList />} />
-                    <Route path={INTERNAL_ROUTES.extrinsics.id.path} element={<Extrinsic />} />
-                  </Route>
-                  <Route path={INTERNAL_ROUTES.accounts.list}>
-                    <Route index element={<AccountList />} />
-                    <Route path={INTERNAL_ROUTES.accounts.id.path} element={<Account />} />
-                  </Route>
-                  <Route path={INTERNAL_ROUTES.events.list}>
-                    <Route index element={<EventList />} />
-                    <Route path={INTERNAL_ROUTES.events.id.path} element={<Event />} />
-                  </Route>
-                  <Route path={INTERNAL_ROUTES.logs.list}>
-                    <Route index element={<LogList />} />
-                    <Route path={INTERNAL_ROUTES.logs.id.path} element={<Log />} />
-                  </Route>
-                </Route>
 
-                <Route element={<NotFound />} path={INTERNAL_ROUTES.notFound} />
-              </Routes>
-            </Container>
-          </ErrorBoundary>
-          <Footer />
-          <Toaster />
-        </Layout>
+                  <Route element={<NotFound />} path={INTERNAL_ROUTES.notFound} />
+                </Routes>
+              </Container>
+            </ErrorBoundary>
+            <Footer />
+            <Toaster />
+          </Layout>
+        </UpdateURLOnRefresh>
       </ScrollToTopWrapper>
     </HashRouter>
   )
