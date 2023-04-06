48ce5b2561528e940852d70daee808af0b78621a
Serge Kovbasiuk <isserge@protonmail.com>
2023-04-06 14:00:15 +0200

    minor

--- health-check/src/utils.ts
+++ health-check/src/utils.ts
@@ -1,7 +1,7 @@
 import { timingSafeEqual } from 'crypto';
 
-export function secureCompare(a: string, b: string | undefined): boolean {
-  if (b === undefined) return false;
+export function secureCompare(a: string | undefined, b: string | undefined): boolean {
+  if (a === undefined || b === undefined) return false;
   const bufferA = Buffer.from(a);
   const bufferB = Buffer.from(b);
   return timingSafeEqual(bufferA, bufferB);
