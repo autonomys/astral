6cefc9d46bdf09b01956efe63198f68affa48bc9
Natacha De la Rosa <delarosa.natacha@gmail.com>
2023-05-02 15:55:22 -0400

    fix(search): fix issues searching anything other than hashes

--- client/src/common/hooks/useSearch.ts
+++ client/src/common/hooks/useSearch.ts
@@ -1,23 +1,11 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useLazyQuery } from '@apollo/client'
+import { gql, useLazyQuery } from '@apollo/client'
 import { isHex } from '@polkadot/util'
 import { isAddress } from '@polkadot/util-crypto'
 
 // common
 import { INTERNAL_ROUTES } from 'common/routes'
-
-// block
-import { QUERY_BLOCK_BY_ID, QUERY_BLOCK_BY_HASH } from 'Block/query'
-
-// account
-import { QUERY_ACCOUNT_BY_ID } from 'Account/query'
-
-// extrinsic
-import { QUERY_EXTRINSIC_BY_HASH, QUERY_EXTRINSIC_BY_ID } from 'Extrinsic/query'
-
-// event
-import { QUERY_EVENT_BY_ID } from 'Event/query'
 import useDomains from 'common/hooks/useDomains'
 import { formatAddress } from 'common/helpers/formatAddress'
 
@@ -28,99 +16,81 @@ type Values = {
 
 const useSearch = (): Values => {
   const [isSearching, setIsSearching] = useState(false)
-  const [searchTerm, setSearchTerm] = useState('')
   const navigate = useNavigate()
   const { selectedChain } = useDomains()
 
-  const [getEvent] = useLazyQuery(QUERY_EVENT_BY_ID, {
-    onCompleted: (data) => {
-      if (data.eventById) {
-        navigate(INTERNAL_ROUTES.events.id.page(selectedChain.urls.page, searchTerm))
-      } else {
-        navigate(INTERNAL_ROUTES.notFound)
-      }
-    },
-  })
-
-  const [getExtrinsicByHash] = useLazyQuery(QUERY_EXTRINSIC_BY_HASH, {
-    onCompleted: (data) => {
-      if (data.extrinsics.length > 0) {
-        if (data.extrinsics.length > 1) {
-          navigate(INTERNAL_ROUTES.search.result.page(selectedChain.urls.page), {
-            state: { extrinsics: data.extrinsics },
-          })
-        } else {
-          const [extrinsic] = data.extrinsics
-          navigate(INTERNAL_ROUTES.extrinsics.id.page(selectedChain.urls.page, extrinsic.id))
+  const [getResults] = useLazyQuery(
+    gql`
+      query GetResults(
+        $term: String!
+        $blockId: BigInt!
+        $isAccount: Boolean!
+        $isBlock: Boolean!
+        $isExtrinsic: Boolean!
+        $isExtrinsicHash: Boolean!
+        $isEvent: Boolean!
+      ) {
+        accountById(id: $term) @include(if: $isAccount) {
+          id
         }
-      } else {
-        navigate(INTERNAL_ROUTES.notFound)
-      }
-    },
-  })
-
-  const [getBlockByHash] = useLazyQuery(QUERY_BLOCK_BY_HASH, {
-    onCompleted: (data) => {
-      if (data.blocks.length > 0) {
-        const [block] = data.blocks
-        navigate(INTERNAL_ROUTES.blocks.id.page(selectedChain.urls.page, block.height))
-      } else {
-        getExtrinsicByHash({ variables: { hash: searchTerm } })
-      }
-    },
-  })
-
-  const [getExtrinsic] = useLazyQuery(QUERY_EXTRINSIC_BY_ID, {
-    onCompleted: (data) => {
-      if (data.extrinsicById) {
-        navigate(INTERNAL_ROUTES.extrinsics.id.page(selectedChain.urls.page, searchTerm))
-      } else {
-        getEvent({ variables: { eventId: searchTerm } })
-      }
-    },
-  })
-
-  const [getBlock] = useLazyQuery(QUERY_BLOCK_BY_ID, {
-    onCompleted: (data) => {
-      if (data.blocks[0] && data.blocks[0].height > 0) {
-        const blockId = Number(searchTerm)
-        navigate(INTERNAL_ROUTES.blocks.id.page(selectedChain.urls.page, blockId))
-      } else {
-        getExtrinsic({ variables: { extrinsicId: searchTerm } })
-      }
-    },
-  })
-
-  const [getAccount] = useLazyQuery(QUERY_ACCOUNT_BY_ID, {
-    onCompleted: (data) => {
-      if (data.accountById) {
-        navigate(INTERNAL_ROUTES.accounts.id.page(selectedChain.urls.page, searchTerm))
-      } else {
-        const blockId = Number(searchTerm)
-        if (isNaN(blockId)) {
-          getExtrinsic({ variables: { extrinsicId: searchTerm } })
-        } else {
-          getBlock({ variables: { blockId: Number(searchTerm) } })
+        blocks(limit: 1, where: { height_eq: $blockId }) @include(if: $isBlock) {
+          height
+        }
+        extrinsicById(id: $term) @include(if: $isExtrinsic) {
+          id
+        }
+        extrinsics(limit: 1, where: { hash_eq: $term }) @include(if: $isExtrinsicHash) {
+          id
+        }
+        eventById(id: $term) @include(if: $isEvent) {
+          id
         }
       }
-    },
-  })
+    `,
+    { fetchPolicy: 'network-only' },
+  )
 
   const handleSearch = (term: string, searchType: number) => {
-    setSearchTerm(term)
     setIsSearching(true)
 
     switch (searchType) {
       case 1: {
         const address = formatAddress(term)
-
         const isHexFormat = isHex(term)
+        const blockId = (!isHexFormat && Number(term)) || term
 
-        if (isHexFormat) {
-          getBlockByHash({ variables: { hash: term } })
-        } else {
-          getAccount({ variables: { accountId: address || searchTerm } })
-        }
+        getResults({
+          variables: {
+            term,
+            blockId: typeof blockId === 'number' ? blockId : -1,
+            isAccount: !isHexFormat && !!address,
+            isExtrinsic: typeof blockId === 'string',
+            isExtrinsicHash: typeof blockId === 'string' && isHexFormat && Boolean(address),
+            isBlock: !isHexFormat && typeof blockId === 'number',
+            isEvent: typeof blockId === 'string',
+          },
+        }).then(({ data }) => {
+          if (data?.accountById) {
+            navigate(INTERNAL_ROUTES.accounts.id.page(selectedChain.urls.page, term))
+          } else if (data?.extrinsicById) {
+            navigate(INTERNAL_ROUTES.extrinsics.id.page(selectedChain.urls.page, term))
+          } else if (data?.extrinsics?.length > 0) {
+            if (data.extrinsics.length > 1) {
+              navigate(INTERNAL_ROUTES.search.result.page(selectedChain.urls.page), {
+                state: { extrinsics: data.extrinsics },
+              })
+            } else {
+              const [extrinsic] = data.extrinsics
+              navigate(INTERNAL_ROUTES.extrinsics.id.page(selectedChain.urls.page, extrinsic.id))
+            }
+          } else if (data?.blocks?.length > 0 && data.blocks[0].height >= 0) {
+            navigate(INTERNAL_ROUTES.blocks.id.page(selectedChain.urls.page, Number(term)))
+          } else {
+            navigate(INTERNAL_ROUTES.notFound)
+          }
+        })
+
+        setIsSearching(false)
 
         break
       }
