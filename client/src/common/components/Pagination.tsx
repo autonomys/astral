250d46910646f2e71ea5a2324625a2279fbee50d
Natacha De la Rosa <delarosa.natacha@gmail.com>
2023-05-09 19:22:37 -0400

    feat(pagination): refactor pagination

--- client/src/common/components/Pagination.tsx
+++ client/src/common/components/Pagination.tsx
@@ -1,8 +1,6 @@
 import { FC } from 'react'
+import ReactPaginate from 'react-paginate'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
-
-//
-import { DOTS, usePagination } from 'common/hooks/usePagination'
 
 type Props = {
   nextPage: () => void
@@ -25,7 +23,7 @@ const Pagination: FC<Props> = ({
   hasPreviousPage,
   handleGetPage,
 }) => {
-  const paginationRange = usePagination({ totalCount, pageSize, currentPage })
+  const pageCount = Math.ceil(totalCount / pageSize)
 
   return (
     <div className='w-full flex items-center justify-between'>
@@ -46,8 +44,27 @@ const Pagination: FC<Props> = ({
         </button>
       </div>
       <div className='hidden sm:flex sm:flex-1 sm:items-center sm:justify-end'>
-        <div>
-          <nav className='isolate inline-flex rounded-md' aria-label='Pagination'>
+        <ReactPaginate
+          forcePage={currentPage}
+          pageCount={pageCount}
+          disabledClassName='disabled'
+          breakLabel={
+            <span className='relative inline-flex items-center bg-white px-4 py-2 text-sm font-medium text-gray-700 rounded-full mx-1 dark:text-white dark:bg-[#1E254E] dark:border-none'>
+              ...
+            </span>
+          }
+          nextLabel={
+            <button
+              onClick={nextPage}
+              disabled={!hasNextPage}
+              className='relative inline-flex items-center rounded-full bg-white px-2 py-2 text-sm font-medium text-[#DE67E4] hover:bg-gray-50 focus:z-20 mr-[14px] dark:text-white dark:bg-[#1E254E] dark:border-none'
+            >
+              <span className='sr-only'>Previous</span>
+              <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
+            </button>
+          }
+          pageRangeDisplayed={3}
+          previousLabel={
             <button
               onClick={previousPage}
               disabled={!hasPreviousPage}
@@ -56,43 +73,24 @@ const Pagination: FC<Props> = ({
               <span className='sr-only'>Previous</span>
               <ChevronLeftIcon className='h-5 w-5' aria-hidden='true' />
             </button>
-            {paginationRange.map((page, index) => {
-              if (page === DOTS) {
-                return (
-                  <span
-                    key={`${page}-${index}`}
-                    className='relative inline-flex items-center bg-white px-4 py-2 text-sm font-medium text-gray-700 rounded-full mx-1 dark:text-white dark:bg-[#1E254E] dark:border-none'
-                  >
-                    ...
-                  </span>
-                )
-              }
-
-              return (
-                <button
-                  key={page}
-                  aria-current='page'
-                  onClick={() => handleGetPage(page)}
-                  className={
-                    page === currentPage
-                      ? 'h-10 min-w-10 relative z-10 inline-flex items-center  bg-black px-4 py-2 text-sm font-medium text-white focus:z-20 rounded-full mx-1 dark:text-white dark:bg-[#DE67E4] dark:border-none'
-                      : 'h-10 min-w-10 relative inline-flex items-center bg-white px-4 py-2 text-sm font-medium text-[#282929] hover:bg-gray-50 focus:z-20 rounded-full mx-1 dark:text-white dark:bg-[#1E254E] dark:border-none'
-                  }
-                >
-                  {page}
-                </button>
-              )
-            })}
+          }
+          pageLabelBuilder={(page) => (
             <button
-              onClick={nextPage}
-              disabled={!hasNextPage}
-              className='relative inline-flex items-center rounded-full bg-white px-2 py-2 text-sm font-medium text-[#DE67E4] hover:bg-gray-50 focus:z-20 ml-[14px] dark:text-white dark:bg-[#1E254E] dark:border-none'
+              key={page}
+              aria-current='page'
+              onClick={() => handleGetPage(page)}
+              className={
+                page === currentPage
+                  ? ' relative z-10 inline-flex items-center  bg-black px-4 py-2 text-sm font-medium text-white focus:z-20 rounded-full mx-1 dark:text-white dark:bg-[#DE67E4] dark:border-none'
+                  : ' relative inline-flex items-center bg-white px-4 py-2 text-sm font-medium text-[#282929] hover:bg-gray-50 focus:z-20 rounded-full mx-1 dark:text-white dark:bg-[#1E254E] dark:border-none'
+              }
             >
-              <span className='sr-only'>Next</span>
-              <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
+              {page}
             </button>
-          </nav>
-        </div>
+          )}
+          renderOnZeroPageCount={null}
+          containerClassName='flex items-center justify-between'
+        />
       </div>
     </div>
   )
