3006308d889921c4824f4f0ad3bed18c6a555865
Natacha De la Rosa <delarosa.natacha@gmail.com>
2023-08-24 12:27:35 +0200

    fix(calc): history calc

--- squid-blockexplorer/src/blocks/utils.ts
+++ squid-blockexplorer/src/blocks/utils.ts
@@ -108,10 +108,9 @@ export function calcSpacePledged(solutionRange: bigint): bigint {
   const SCALAR = BigInt(32);
 
   const history_size =
-    ((((((MAX_U64 * SLOT_PROBABILITY[0]) /
+    ((((MAX_U64 * SLOT_PROBABILITY[0]) /
       SLOT_PROBABILITY[1] /
-      solutionRange) *
-      PIECE_SIZE) /
+      (solutionRange * PIECE_SIZE) /
       RECORD_BUCKETS) *
       RECORD_CHUNKS) /
       SOLUTION_RANGE) *
