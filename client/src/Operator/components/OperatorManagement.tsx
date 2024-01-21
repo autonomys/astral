16341ae7de338cb5a8b9b3a61bb8a913e5f4707b
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-01-21 18:45:02 -0500

    Add actions dropdown

--- client/src/Operator/components/OperatorManagement.tsx
+++ client/src/Operator/components/OperatorManagement.tsx
@@ -1,7 +1,9 @@
 import { useApolloClient, useQuery } from '@apollo/client'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
 import { SortingState } from '@tanstack/react-table'
 import { Operator } from 'gql/graphql'
-import { FC, useCallback, useEffect, useMemo, useState } from 'react'
+import { FC, Fragment, useCallback, useEffect, useMemo, useState } from 'react'
 import { useErrorHandler } from 'react-error-boundary'
 import { Link } from 'react-router-dom'
 
@@ -21,6 +23,18 @@ import { QUERY_OPERATOR_CONNECTION_LIST } from 'Operator/query'
 import { NotFound } from 'layout/components'
 import OperatorsListCard from './OperatorsListCard'
 
+enum OperatorActionType {
+  None = 'none',
+  AddFunds = 'Add Funds',
+  Withdraw = 'Withdraw',
+  Deregister = 'Deregister',
+}
+
+type OperatorAction = {
+  type: OperatorActionType
+  operatorId: number | null
+}
+
 const OperatorManagement: FC = () => {
   const [searchOperator, setSearch] = useState<string>('')
   const [sorting, setSorting] = useState<SortingState>([{ id: 'id', desc: false }])
@@ -34,9 +48,14 @@ const OperatorManagement: FC = () => {
   const { selectedChain, selectedDomain } = useDomains()
   const apolloClient = useApolloClient()
 
+  const [action, setAction] = useState<OperatorAction>({
+    type: OperatorActionType.None,
+    operatorId: null,
+  })
+
   const cols = useMemo(
-    () => createColumns(selectedDomain, selectedChain.urls.page),
-    [selectedDomain, selectedChain],
+    () => createColumns(selectedDomain, selectedChain.urls.page, action, setAction),
+    [selectedDomain, selectedChain.urls.page, action],
   )
 
   const variables = useMemo(
@@ -294,7 +313,7 @@ const OperatorManagement: FC = () => {
 
 export default OperatorManagement
 
-const createColumns = (selectedDomain, chain) => {
+const createColumns = (selectedDomain, chain, action, setAction) => {
   return [
     {
       accessorKey: 'id',
@@ -347,12 +366,6 @@ const createColumns = (selectedDomain, chain) => {
       cell: ({ row }) => <div>{`${bigNumberToNumber(row.original.currentTotalStake)} tSSC`}</div>,
     },
     {
-      accessorKey: 'totalShares',
-      header: 'Total Shares',
-      enableSorting: true,
-      cell: ({ row }) => <div>{numberWithCommas(row.original.totalShares)}</div>,
-    },
-    {
       accessorKey: 'nominators',
       header: 'Nominators',
       enableSorting: false,
@@ -365,6 +378,82 @@ const createColumns = (selectedDomain, chain) => {
       header: 'Status',
       enableSorting: true,
       cell: ({ row }) => <div>{row.original.status}</div>,
+    },
+    {
+      accessorKey: 'actions',
+      header: 'Actions',
+      enableSorting: false,
+      cell: ({ row }) => (
+        <Listbox
+          value={action.type}
+          onChange={(val) =>
+            setAction({
+              type: val,
+              operatorId: row.original.id,
+            })
+          }
+        >
+          <div className='relative'>
+            <Listbox.Button className='font-["Montserrat"] relative w-full cursor-default mt-4 rounded-full bg-[#DE67E4] text-white py-[10px] pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm dark:bg-gradient-to-r from-[#EA71F9] to-[#4D397A] dark:text-white'>
+              <div className='flex items-center justify-center'>
+                <span className='hidden sm:block ml-2 truncate w-5 text-sm md:w-full '>
+                  Actions
+                </span>
+                <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+                  <ChevronDownIcon
+                    className='h-5 w-5 text-gray-400 ui-open:rotate-180 ui-open:transform dark:text-[#DE67E4]'
+                    aria-hidden='true'
+                  />
+                </span>
+              </div>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave='transition ease-in duration-100'
+              leaveFrom='opacity-100'
+              leaveTo='opacity-0'
+            >
+              <Listbox.Options className='absolute mt-1 max-h-60 w-auto md:w-full overflow-auto rounded-xl bg-white py-2 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm dark:bg-[#1E254E] dark:text-white'>
+                {Object.keys(OperatorActionType)
+                  .slice(1)
+                  .map((actionType, index) => (
+                    <Listbox.Option
+                      key={index}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 text-gray-900 md:pl-4 pr-4 dark:text-white ${
+                          active && 'bg-gray-100 dark:bg-[#2A345E]'
+                        }`
+                      }
+                      value={actionType}
+                    >
+                      {({ selected }) => {
+                        return (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? 'font-medium' : 'font-normal'
+                              } ${
+                                OperatorActionType[actionType] === OperatorActionType.Deregister &&
+                                'text-red-500'
+                              }`}
+                            >
+                              {OperatorActionType[actionType]}
+                            </span>
+                            {selected ? (
+                              <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-[#37D058]'>
+                                <CheckIcon className='h-5 w-5 hidden md:block' aria-hidden='true' />
+                              </span>
+                            ) : null}
+                          </>
+                        )
+                      }}
+                    </Listbox.Option>
+                  ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
+      ),
     },
   ]
 }
