a083b48b48d98faed920e5e71bd0beca2435d345
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-01-21 20:37:36 -0500

    Add Add Funds and Withdraw logic

--- client/src/Operator/components/ActionsModal.tsx
+++ client/src/Operator/components/ActionsModal.tsx
@@ -1,6 +1,8 @@
 import Modal from 'common/components/Modal'
 import useWallet from 'common/hooks/useWallet'
-import { FC, useCallback, useMemo, useState } from 'react'
+import { Field, Form, Formik, FormikState } from 'formik'
+import { FC, useCallback, useEffect, useMemo, useState } from 'react'
+import * as Yup from 'yup'
 
 export enum OperatorActionType {
   None = 'none',
@@ -20,9 +22,95 @@ type Props = {
   onClose: () => void
 }
 
+interface FormValues {
+  amount: number
+}
+
 export const ActionsModal: FC<Props> = ({ isOpen, action, onClose }) => {
   const { api, actingAccount, injector } = useWallet()
   const [formError, setFormError] = useState<string | null>(null)
+  const [tokenDecimals, setTokenDecimals] = useState<number>(0)
+  const [tokenSymbol, setTokenSymbol] = useState<string>('')
+
+  const initialValues: FormValues = useMemo(
+    () => ({
+      amount: 0,
+    }),
+    [],
+  )
+
+  const fundsFormValidationSchema = Yup.object().shape({
+    amount: Yup.number()
+      .min(0, `Amount  need to be greater than 0 ${tokenSymbol}`)
+      .required('Amount to stake is required'),
+  })
+
+  const loadData = useCallback(async () => {
+    if (!api) return
+
+    const properties = await api.rpc.system.properties()
+    setTokenDecimals((properties.tokenDecimals.toPrimitive() as number[])[0])
+    setTokenSymbol((properties.tokenSymbol.toJSON() as string[])[0])
+  }, [api])
+
+  const handleClose = useCallback(() => {
+    setFormError(null)
+    onClose()
+  }, [onClose])
+
+  const handleAddFunds = useCallback(
+    async (
+      values: FormValues,
+      resetForm: (nextState?: Partial<FormikState<FormValues>> | undefined) => void,
+    ) => {
+      if (!api || !actingAccount || !injector)
+        return setFormError('We are not able to connect to the blockchain')
+
+      try {
+        const block = await api.rpc.chain.getBlock()
+        const hash = await api.tx.domains
+          .nominateOperator(action.operatorId, values.amount * 10 ** tokenDecimals)
+          .signAndSend(actingAccount.address, { signer: injector.signer })
+
+        console.log('block', block)
+        console.log('hash', hash)
+
+        resetForm()
+        handleClose()
+      } catch (error) {
+        setFormError('There was an error while adding funds to the operator')
+        console.error('Error', error)
+      }
+    },
+    [api, actingAccount, injector, action.operatorId, tokenDecimals, handleClose],
+  )
+
+  const handleWithdraw = useCallback(
+    async (
+      values: FormValues,
+      resetForm: (nextState?: Partial<FormikState<FormValues>> | undefined) => void,
+    ) => {
+      if (!api || !actingAccount || !injector)
+        return setFormError('We are not able to connect to the blockchain')
+
+      try {
+        const block = await api.rpc.chain.getBlock()
+        const hash = await api.tx.domains
+          .withdrawStake(action.operatorId, { Some: values.amount * 10 ** tokenDecimals }) // amount === 'max' ? { All: null } : { Some: amount }
+          .signAndSend(actingAccount.address, { signer: injector.signer })
+
+        console.log('block', block)
+        console.log('hash', hash)
+
+        resetForm()
+        handleClose()
+      } catch (error) {
+        setFormError('There was an error while withdraw funds from the operator')
+        console.error('Error', error)
+      }
+    },
+    [api, actingAccount, injector, action.operatorId, tokenDecimals, handleClose],
+  )
 
   const handleDeregister = useCallback(async () => {
     if (!api || !actingAccount || !injector)
@@ -37,17 +125,17 @@ export const ActionsModal: FC<Props> = ({ isOpen, action, onClose }) => {
       console.log('block', block)
       console.log('hash', hash)
 
-      onClose()
+      handleClose()
     } catch (error) {
       setFormError('There was an error while de-registering the operator')
       console.error('Error', error)
     }
-  }, [actingAccount, action.operatorId, api, injector, onClose])
+  }, [actingAccount, action.operatorId, api, injector, handleClose])
 
   const ErrorPlaceholder = useMemo(
     () =>
       formError ? (
-        <div className='text-red-500 text-md h-4' data-testid='errorMessage'>
+        <div className='text-red-500 text-md h-8' data-testid='errorMessage'>
           {formError}
         </div>
       ) : (
@@ -57,11 +145,73 @@ export const ActionsModal: FC<Props> = ({ isOpen, action, onClose }) => {
   )
 
   const ActionBody = useMemo(() => {
-    switch (action.type) {
+    switch (OperatorActionType[action.type]) {
       case OperatorActionType.AddFunds:
-        return <>Add Funds</>
       case OperatorActionType.Withdraw:
-        return <>Withdraw</>
+        return (
+          <div className='flex flex-col gap-4 items-start'>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={fundsFormValidationSchema}
+              onSubmit={(values, { resetForm }) =>
+                OperatorActionType[action.type] === OperatorActionType.AddFunds
+                  ? handleAddFunds(values, resetForm)
+                  : handleWithdraw(values, resetForm)
+              }
+            >
+              {({ errors, touched, handleSubmit }) => (
+                <Form
+                  className='w-full'
+                  onSubmit={handleSubmit}
+                  data-testid='testOperatorStakeForm'
+                >
+                  <span className='text-[#241235] text-base font-medium dark:text-white'>
+                    {`Amount to ${
+                      OperatorActionType[action.type] === OperatorActionType.AddFunds
+                        ? 'stake'
+                        : 'withdraw'
+                    }`}
+                  </span>
+                  <Field
+                    name='amount'
+                    placeholder={`Amount to ${
+                      OperatorActionType[action.type] === OperatorActionType.AddFunds
+                        ? 'stake'
+                        : 'withdraw'
+                    }`}
+                    className={`dark:bg-[#1E254E] dark:text-white block px-4 py-[10px] mt-4 w-full text-sm text-gray-900 rounded-xl bg-white shadow-lg
+                            ${
+                              errors.amount &&
+                              touched.amount &&
+                              'block px-4 py-[10px] w-full text-sm text-gray-900 rounded-full bg-white shadow-lg'
+                            }
+                          `}
+                  />
+                  {errors.amount && touched.amount ? (
+                    <div className='text-red-500 text-md mt-2 h-8' data-testid='errorMessage'>
+                      {errors.amount}
+                    </div>
+                  ) : (
+                    <div className='text-md mt-2 h-8' data-testid='placeHolder' />
+                  )}
+                  {ErrorPlaceholder}
+                  {!actingAccount ? (
+                    <div className='text-red-500 text-md mt-2 h-8' data-testid='errorMessage'>
+                      You need to connect your wallet
+                    </div>
+                  ) : (
+                    <button
+                      className='w-full max-w-fit flex px-2 gap-2 text-sm md:text-base items-center md:space-x-4 rounded-full bg-[#241235] text-white font-medium dark:bg-[#DE67E4]'
+                      type='submit'
+                    >
+                      {OperatorActionType[action.type]}
+                    </button>
+                  )}
+                </Form>
+              )}
+            </Formik>
+          </div>
+        )
       case OperatorActionType.Deregister:
         return (
           <div className='flex flex-col gap-4 items-start'>
@@ -73,17 +223,30 @@ export const ActionsModal: FC<Props> = ({ isOpen, action, onClose }) => {
               className='w-full max-w-fit flex px-2 gap-2 text-sm md:text-base items-center md:space-x-4 rounded-full bg-red-500 text-white font-medium dark:bg-red-500'
               onClick={handleDeregister}
             >
-              {action.type}
+              {OperatorActionType[action.type]}
             </button>
           </div>
         )
       default:
         return null
     }
-  }, [ErrorPlaceholder, action.type, handleDeregister])
+  }, [
+    ErrorPlaceholder,
+    actingAccount,
+    action.type,
+    fundsFormValidationSchema,
+    handleAddFunds,
+    handleDeregister,
+    handleWithdraw,
+    initialValues,
+  ])
+
+  useEffect(() => {
+    loadData()
+  }, [api, loadData])
 
   return (
-    <Modal title={action.type} onClose={onClose} isOpen={isOpen}>
+    <Modal title={action.type} onClose={handleClose} isOpen={isOpen}>
       <div className='flex flex-col gap-4 items-start'>
         <div className='flex flex-col gap-4 items-center'>
           <div className='grid grid-cols-1 gap-4'>{ActionBody}</div>
