279b40de4c9ca6f5556b8d100a4726fc21f5f34b
Natacha De la Rosa <delarosa.natacha@gmail.com>
2024-02-16 16:18:31 +0100

    fix(loading): loading issue

--- client/src/layout/components/Layout.tsx
+++ client/src/layout/components/Layout.tsx
@@ -1,10 +1,18 @@
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useEffect } from 'react'
+import ReactGA from 'react-ga'
+import { useLocation } from 'react-router-dom'
 
 type Props = {
   children?: ReactNode
 }
 
 const MainLayout: FC<Props> = ({ children }) => {
+  const location = useLocation()
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search)
+  }, [location])
+
   return (
     <div className="flex flex-col min-h-screen w-full bg-gradient-to-b from-[#F1F7F8] to-[#EFFDFF] dark:bg-dark font-['Montserrat'] relative">
       <div className="flex flex-col min-h-screen w-full bg-[url('/src/common/images/backgroundColor.svg')] bg-cover font-['Montserrat'] relative">
