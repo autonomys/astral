a53a0c28d117d5dcbce36bcf1ad12723a197140b
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-02-22 09:07:41 -0500

    improve header memoization

--- client/src/layout/components/DomainHeader.tsx
+++ client/src/layout/components/DomainHeader.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useState } from 'react'
+import { FC, useCallback, useMemo, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 // layout
@@ -56,6 +56,28 @@ const DomainHeader: FC = () => {
     [setWalletSidekickIsOpen],
   )
 
+  const domainsOptions = useMemo(
+    () =>
+      DOMAINS.map((item, index) => {
+        const isActive = pathName.includes(`${selectedChain.urls.page}/${item.name}`)
+        return (
+          <div className='text-[13px] font-semibold items-center flex' key={`${item}-${index}`}>
+            <button
+              onClick={() => handleDomainSelected(item.name)}
+              className={
+                isActive
+                  ? 'bg-[#241235] rounded-full py-2 px-4 dark:bg-[#DE67E4] text-white'
+                  : 'bg-white text-[#282929] dark:text-white dark:bg-[#1E254E]'
+              }
+            >
+              {item.title}
+            </button>
+          </div>
+        )
+      }),
+    [handleDomainSelected, pathName, selectedChain.urls.page],
+  )
+
   return (
     <div
       className='w-full h-[60px] bg-white dark:bg-[#1E254E] z-10'
@@ -63,25 +85,7 @@ const DomainHeader: FC = () => {
       data-accordion='open'
     >
       <div className='w-full flex justify-between container py-3 items-center px-5 md:px-[25px] 2xl:px-0 mx-auto'>
-        <div className='flex gap-9'>
-          {DOMAINS.map((item, index) => {
-            const isActive = pathName.includes(`${selectedChain.urls.page}/${item.name}`)
-            return (
-              <div className='text-[13px] font-semibold items-center flex' key={`${item}-${index}`}>
-                <button
-                  onClick={() => handleDomainSelected(item.name)}
-                  className={
-                    isActive
-                      ? 'bg-[#241235] rounded-full py-2 px-4 dark:bg-[#DE67E4] text-white'
-                      : 'bg-white text-[#282929] dark:text-white dark:bg-[#1E254E]'
-                  }
-                >
-                  {item.title}
-                </button>
-              </div>
-            )
-          })}
-        </div>
+        <div className='flex gap-9'>{domainsOptions}</div>
         <div className='flex gap-4'>
           {!actingAccount ? (
             <button
