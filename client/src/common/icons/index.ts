3a6a5287bda3865da87d3dff8f30b05657b15086
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-02-22 09:03:13 -0500

    add copy icon

--- client/src/common/icons/index.ts
+++ client/src/common/icons/index.ts
@@ -1,19 +1,21 @@
 import BlockIcon from 'common/icons/BlockIcon'
+import CopyIcon from 'common/icons/CopyIcon'
 import DocIcon from 'common/icons/DocIcon'
-import WalletIcon from 'common/icons/WalletIcon'
-import PieChartIcon from 'common/icons/PieChartIcon'
-import LogoIcon from 'common/icons/LogoIcon'
-import SubspaceSymbol from 'common/icons/SubspaceSymbol'
-import FilterIcon from 'common/icons/FilterIcon'
 import EmptyCircleIcon from 'common/icons/EmptyCircleIcon'
+import FilterIcon from 'common/icons/FilterIcon'
+import LogoIcon from 'common/icons/LogoIcon'
+import PieChartIcon from 'common/icons/PieChartIcon'
+import SubspaceSymbol from 'common/icons/SubspaceSymbol'
+import WalletIcon from 'common/icons/WalletIcon'
 
 export {
   BlockIcon,
+  CopyIcon,
   DocIcon,
-  WalletIcon,
-  PieChartIcon,
-  LogoIcon,
-  SubspaceSymbol,
-  FilterIcon,
   EmptyCircleIcon,
+  FilterIcon,
+  LogoIcon,
+  PieChartIcon,
+  SubspaceSymbol,
+  WalletIcon,
 }
