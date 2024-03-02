a3be3623716caad4796afd2a1a802ef73b84d41f
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-03-02 11:12:51 -0500

    allow to close&hide the indexing error

--- client/src/common/components/IndexingError.tsx
+++ client/src/common/components/IndexingError.tsx
@@ -1,4 +1,15 @@
-const IndexingError = () => {
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { FC, useCallback, useState } from 'react'
+
+const IndexingError: FC = () => {
+  const [isWarningVisible, setIsWarningVisible] = useState(true)
+
+  const handleHideWarning = useCallback(() => {
+    setIsWarningVisible(false)
+  }, [])
+
+  if (!isWarningVisible) return <></>
+
   return (
     <div className='w-full flex items-center justify-center bg-orange-600/40 py-2 px-5'>
       <div className='text-gray-600 text-sm font-medium dark:text-white'>
@@ -7,6 +18,12 @@ const IndexingError = () => {
           work to resolve this issue.
         </p>
       </div>
+      <button
+        className='mx-2 w-8 px-1 py-1 bg-white dark:bg-orange-600/40 hover:bg-gray-200 text-gray-800 dark:text-white text-sm font-medium rounded-[20px]'
+        onClick={handleHideWarning}
+      >
+        <XMarkIcon className='w-6 h-6 text-[#282929] dark:text-white' />
+      </button>
     </div>
   )
 }
