a97fa4838693f29ec8c61eec6b9cbbf2e646fe2f
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-03-02 12:03:12 -0500

    simplify naming

--- client/src/common/components/ConnectWalletButton.tsx
+++ client/src/common/components/ConnectWalletButton.tsx
@@ -4,26 +4,26 @@ import React, { useCallback, useState } from 'react'
 import PreferredExtensionModal from '../../layout/components/PreferredExtensionModal'
 
 export const ConnectWalletButton: React.FC = () => {
-  const [walletModalIsOpen, setWalletModalIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
   const isDesktop = useMediaQuery('(min-width: 1024px)')
 
-  const handleConnectWallet = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const onClick = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault()
-    setWalletModalIsOpen(true)
+    setIsOpen(true)
   }, [])
-  const handleWalletModalOnClose = useCallback(() => setWalletModalIsOpen(false), [])
+  const onClose = useCallback(() => setIsOpen(false), [])
 
   return (
     <>
       <button
-        onClick={handleConnectWallet}
+        onClick={onClick}
         className={`h-10 ${
           isDesktop ? 'w-36' : 'w-10 py-2 px-2'
         } text-white font-medium bg-gradient-to-r from-[#EA71F9] to-[#4D397A] rounded-full`}
       >
         {isDesktop ? 'Connect Wallet' : <WalletIcon className='w-6 h-6' />}
       </button>
-      <PreferredExtensionModal isOpen={walletModalIsOpen} onClose={handleWalletModalOnClose} />
+      <PreferredExtensionModal isOpen={isOpen} onClose={onClose} />
     </>
   )
 }
