9f7b9eafc5d4e58917c8489970203a7bf629c5e8
Natacha De la Rosa <delarosa.natacha@gmail.com>
2024-03-25 15:50:40 +0100

    Update processor.ts

--- rewards-squid/src/processor.ts
+++ rewards-squid/src/processor.ts
@@ -17,7 +17,7 @@ export const processor = new SubstrateBatchProcessor()
     url: assertNotNull(process.env.RPC_ENDPOINT),
     rateLimit: 10,
   })
-  .setBlockRange({ from: 341619 })
+  .setBlockRange({ from: 0 })
   .addEvent({
     name: [
       events.domains.storageFeeDeposited.name,
