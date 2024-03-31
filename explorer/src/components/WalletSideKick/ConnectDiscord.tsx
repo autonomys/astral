6da3161e0d516b1c54696667a699a2d6a9e36499
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-03-31 09:51:09 -0400

    improve ConnectDiscord

--- explorer/src/components/WalletSideKick/ConnectDiscord.tsx
+++ explorer/src/components/WalletSideKick/ConnectDiscord.tsx
@@ -2,7 +2,8 @@ import { useQuery } from '@apollo/client'
 import { Accordion } from 'components/common/Accordion'
 import { List, StyledListItem } from 'components/common/List'
 import { CheckRoleQuery } from 'gql/graphql'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
+import Link from 'next/link'
 import { FC, useCallback, useMemo } from 'react'
 import { QUERY_CHECK_ROLE } from './query'
 
@@ -11,6 +12,8 @@ interface StakingSummaryProps {
 }
 
 export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => {
+  const { data: session } = useSession()
+
   const summaryVariables = useMemo(
     () => ({
       first: 10,
@@ -18,14 +21,15 @@ export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => 
     }),
     [subspaceAccount],
   )
-  const { data, error, loading } = useQuery<CheckRoleQuery>(QUERY_CHECK_ROLE, {
+  const { data } = useQuery<CheckRoleQuery>(QUERY_CHECK_ROLE, {
     variables: summaryVariables,
     pollInterval: 6000,
   })
 
-  const handleConnectDiscord = useCallback(() => {
+  const handleConnectDiscord = useCallback(async () => {
     console.log('Connect Discord')
-    signIn('discord')
+    const liginDiscord = await signIn('discord')
+    console.log('liginDiscord', liginDiscord)
   }, [])
 
   const handleSignAndSendProof = useCallback(() => {
@@ -39,15 +43,38 @@ export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => 
       <div className='m-2 mt-0 rounded-[20px] bg-[#DDEFF1] p-5 dark:bg-[#1E254E] dark:text-white'>
         <Accordion title='Get your Farmer role on Discord'>
           <List>
-            <StyledListItem title='1. Connect your Discord'>
-              <button
-                className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'
-                onClick={handleConnectDiscord}
-              >
-                Connect
-              </button>
-            </StyledListItem>
-            <StyledListItem title='2. Sign a message as proof to request role'>
+            {session && session.user && !session.user.isDiscordGuildMember ? (
+              <>
+                <StyledListItem title='1. Join our Discord server'>
+                  <Link href={process.env.NEXT_PUBLIC_DISCORD_INVITE_URL ?? ''} target='_blank'>
+                    <button className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'>
+                      Join
+                    </button>
+                  </Link>
+                </StyledListItem>
+
+                <StyledListItem title='2. Connect your Discord again!'>
+                  <button
+                    className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'
+                    onClick={handleConnectDiscord}
+                  >
+                    Connect
+                  </button>
+                </StyledListItem>
+              </>
+            ) : (
+              <StyledListItem title='1. Connect your Discord'>
+                <button
+                  className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'
+                  onClick={handleConnectDiscord}
+                >
+                  Connect
+                </button>
+              </StyledListItem>
+            )}
+            <StyledListItem
+              title={`${session && session.user && !session.user.isDiscordGuildMember ? 3 : 2}. Sign a message as proof to request role`}
+            >
               <button
                 className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'
                 onClick={handleSignAndSendProof}
