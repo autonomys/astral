c3562509e495c0d0c174cc8944ec11f3652152de
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-04-27 07:49:42 -0400

    cleanup discord auth

--- explorer/src/utils/auth/providers/discord.ts
+++ explorer/src/utils/auth/providers/discord.ts
@@ -1,5 +1,3 @@
-import { chains } from '@/constants'
-import { QUERY_CHECK_ROLE } from 'components/WalletSideKick/query'
 import * as jsonwebtoken from 'jsonwebtoken'
 import type { TokenSet } from 'next-auth'
 import { JWT } from 'next-auth/jwt'
@@ -27,7 +25,6 @@ export const Discord = () => {
       const { NEXTAUTH_SECRET } = process.env
 
       const { get } = cookies()
-      const selectedChain = get('selectedChain')
       const sessionToken = get('next-auth.session-token')?.value || ''
       const session = jsonwebtoken.verify(sessionToken, NEXTAUTH_SECRET, {
         algorithms: ['HS256'],
@@ -36,37 +33,7 @@ export const Discord = () => {
       try {
         const isDiscordGuildMember = await verifyDiscordGuildMember(token.access_token)
 
-        let isDiscordFarmerRole = false
-
-        // To-Do: Implement VCs (in a separate file)
-        // try {
-        //   if (!QUERY_CHECK_ROLE.loc) throw new Error('No query')
-        //   if (!subspaceAccount) throw new Error('No subspaceAccount')
-        //   if (!selectedChain) throw new Error('No selected chain')
-        //   const api = chains.find((chain) => chain.urls.page === selectedChain.value)
-        //   if (!api) throw new Error('No selected chain api')
-        //   const request = await fetch(api.urls.api, {
-        //     method: 'POST',
-        //     headers: {
-        //       'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({
-        //       query: QUERY_CHECK_ROLE.loc.source.body,
-        //       variables: {
-        //         subspaceAccount: subspaceAccount.value,
-        //       },
-        //     }),
-        //   })
-        //   const { data } = await request.json()
-        //   isDiscordFarmerRole = data && data.isFarmer && data.isFarmer.length > 0
-        // } catch (error) {
-        //   console.error('Failed to fetch if user has farmer role on Discord:', error)
-        //   throw new Error('Failed to fetch if user has farmer role on Discord')
-        // }
-
         const did = 'did:openid:discord:' + profile.id
-
-        console.log('did', did)
 
         return {
           id: session.id || did,
@@ -79,7 +46,7 @@ export const Discord = () => {
               member: isDiscordGuildMember,
               // To-Do: Implement role VCs
               roles: {
-                farmer: isDiscordFarmerRole,
+                farmer: false,
                 operator: false,
                 nominator: false,
               },
