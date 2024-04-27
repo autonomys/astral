4fcec4d6b06836438a990187f3992300af1725e0
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-04-26 20:15:03 -0400

    add sign wallet ownership flow

--- explorer/src/components/WalletSideKick/ConnectDiscord.tsx
+++ explorer/src/components/WalletSideKick/ConnectDiscord.tsx
@@ -2,9 +2,11 @@ import { useQuery } from '@apollo/client'
 import { Accordion } from 'components/common/Accordion'
 import { List, StyledListItem } from 'components/common/List'
 import { CheckRoleQuery } from 'gql/graphql'
+import useWallet from 'hooks/useWallet'
 import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { FC, useCallback, useMemo } from 'react'
+import toast from 'react-hot-toast'
 import { QUERY_CHECK_ROLE } from './query'
 
 interface StakingSummaryProps {
@@ -13,6 +15,7 @@ interface StakingSummaryProps {
 
 export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => {
   const { data: session } = useSession()
+  const { actingAccount, injector } = useWallet()
 
   const summaryVariables = useMemo(
     () => ({
@@ -26,6 +29,34 @@ export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => 
     pollInterval: 6000,
   })
 
+  const handleWalletOwnership = useCallback(async () => {
+    if (!actingAccount || !injector) throw new Error('No wallet connected')
+    try {
+      const message = `I am the owner of ${subspaceAccount}`
+      const signature =
+        injector.signer.signRaw &&
+        (await injector.signer.signRaw({
+          address: actingAccount.address,
+          type: 'bytes',
+          data: message,
+        }))
+
+      if (!signature) throw new Error('No signature')
+
+      await signIn('subspace', {
+        account: subspaceAccount,
+        message,
+        signature: signature.signature,
+      })
+
+      toast.success('The message was signed', { position: 'bottom-center' })
+    } catch (error) {
+      const reason = 'There was an error while signing the message'
+      toast.error(reason, { position: 'bottom-center' })
+      console.error('Error', error)
+    }
+  }, [actingAccount, injector, subspaceAccount])
+
   const handleConnectDiscord = useCallback(async () => await signIn('discord'), [])
 
   const handleSignAndSendProof = useCallback(async () => {
@@ -38,8 +69,9 @@ export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => 
   if (
     session &&
     session.user &&
-    session.user.isDiscordFarmerRole &&
-    session.user.isDiscordFarmerRole
+    session.user.discord &&
+    session.user.discord.isDiscordFarmerRole &&
+    session.user.discord.isDiscordFarmerRole
   )
     return (
       <div className='m-2 mt-0 rounded-[20px] bg-[#DDEFF1] p-5 dark:bg-[#1E254E] dark:text-white'>
@@ -56,9 +88,20 @@ export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => 
       <div className='m-2 mt-0 rounded-[20px] bg-[#DDEFF1] p-5 dark:bg-[#1E254E] dark:text-white'>
         <Accordion title='Get your Farmer role on Discord'>
           <List>
-            {session && session.user && !session.user.isDiscordGuildMember ? (
+            <StyledListItem title='1. Verify the ownership of your wallet'>
+              <button
+                className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'
+                onClick={handleWalletOwnership}
+              >
+                Sign
+              </button>
+            </StyledListItem>
+            {session &&
+            session.user &&
+            session.user.discord &&
+            !session.user.discord.isDiscordGuildMember ? (
               <>
-                <StyledListItem title='1. Join our Discord server'>
+                <StyledListItem title='2. Join our Discord server'>
                   <Link href={process.env.NEXT_PUBLIC_DISCORD_INVITE_URL ?? ''} target='_blank'>
                     <button className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'>
                       Join
@@ -66,7 +109,7 @@ export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => 
                   </Link>
                 </StyledListItem>
 
-                <StyledListItem title='2. Connect your Discord again!'>
+                <StyledListItem title='3. Connect your Discord again!'>
                   <button
                     className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'
                     onClick={handleConnectDiscord}
@@ -76,7 +119,7 @@ export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => 
                 </StyledListItem>
               </>
             ) : (
-              <StyledListItem title='1. Connect your Discord'>
+              <StyledListItem title='2. Connect your Discord'>
                 <button
                   className='w-[100px] rounded-xl border border-[#DE67E4] bg-transparent px-4 shadow-lg'
                   onClick={handleConnectDiscord}
@@ -86,7 +129,7 @@ export const ConnectDiscord: FC<StakingSummaryProps> = ({ subspaceAccount }) => 
               </StyledListItem>
             )}
             <StyledListItem
-              title={`${session && session.user && !session.user.isDiscordGuildMember ? 3 : 2}. Sign a message as proof to request role`}
+              title={`${session && session.user && session.user.discord && !session.user.discord.isDiscordGuildMember ? 3 : 2}. Sign a message as proof to request role`}
             >
               <button
                 className={
