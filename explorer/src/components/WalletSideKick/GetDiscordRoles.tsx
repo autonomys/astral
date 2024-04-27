bf5035fbfc05670c986573655afee05cdf5c6738
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-04-27 10:40:29 -0400

    improve error handling on signing

--- explorer/src/components/WalletSideKick/GetDiscordRoles.tsx
+++ explorer/src/components/WalletSideKick/GetDiscordRoles.tsx
@@ -31,27 +31,27 @@ export const GetDiscordRoles: FC<StakingSummaryProps> = ({ subspaceAccount }) =>
   })
 
   const handleWalletOwnership = useCallback(async () => {
-    if (!actingAccount || !injector) throw new Error('No wallet connected')
     try {
-      const message = `I am the owner of ${subspaceAccount}`
-      const signature =
-        injector.signer.signRaw &&
-        (await injector.signer.signRaw({
-          address: actingAccount.address,
-          type: 'bytes',
-          data: message,
-        }))
+      if (!actingAccount || !injector) throw new Error('No wallet connected')
+      if (!injector.signer.signRaw) throw new Error('No signer')
 
+      // Prepare and sign the message
+      const message = `I am the owner of ${subspaceAccount}`
+      const signature = await injector.signer.signRaw({
+        address: actingAccount.address,
+        type: 'bytes',
+        data: message,
+      })
       if (!signature) throw new Error('No signature')
 
+      // Sign-in using the message&signature
       await signIn('subspace', {
         account: subspaceAccount,
         message,
         signature: signature.signature,
         redirect: false,
       })
-
-      toast.success('The message was signed', { position: 'bottom-center' })
+      toast.success('You verified the ownership of your wallet!', { position: 'bottom-center' })
     } catch (error) {
       const reason = 'There was an error while signing the message'
       toast.error(reason, { position: 'bottom-center' })
