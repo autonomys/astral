2e35cb2dc39b6c04d33c9524fce7ac727760cdde
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-04-27 07:49:19 -0400

    add queryGraphqlServer utils

---
+++ explorer/src/utils/queryGraphqlServer.ts
@@ -0,0 +1,34 @@
+import { chains } from 'constants/'
+import { cookies } from 'next/headers'
+
+export const queryGraphqlServer = async (query: string, variables: object) => {
+  try {
+    // Get the selected chain from the cookies
+    const { get } = cookies()
+    const selectedChain = get('selectedChain')
+    if (!selectedChain) throw new Error('No selected chain')
+
+    // Find the selected chain api
+    const api = chains.find((chain) => chain.urls.page === selectedChain.value)
+    if (!api) throw new Error('No selected chain api')
+
+    // Fetch the data from the api
+    const request = await fetch(api.urls.api, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    })
+    const { data } = await request.json()
+
+    // Return the data
+    return data
+  } catch (error) {
+    console.error('Failed to fetch Astral Subsquid:', error)
+    throw new Error('Failed to fetch Astral Subsquid')
+  }
+}
