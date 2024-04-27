0fdbbe0bdfde83ccf2b35a53f9e7da1caa32c230
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-04-26 21:02:54 -0400

    clean-up auth options

--- explorer/src/utils/auth/authOptions.ts
+++ explorer/src/utils/auth/authOptions.ts
@@ -1,3 +1,4 @@
+import { TOKEN_EXPIRATION } from 'constants/session'
 import { sign, verify } from 'jsonwebtoken'
 import { AuthOptions } from 'next-auth'
 import { JWT } from 'next-auth/jwt'
@@ -6,34 +7,29 @@ import { providers } from './providers'
 const { NEXTAUTH_SECRET } = process.env
 
 export const authOptions: AuthOptions = {
-  debug: false,
+  debug: true,
   secret: NEXTAUTH_SECRET,
   providers,
-  session: { strategy: 'jwt' },
+  session: {
+    strategy: 'jwt',
+    maxAge: TOKEN_EXPIRATION,
+  },
   jwt: {
     encode: async ({ token, secret }) => sign(token!, secret, { algorithm: 'HS256' }),
     decode: async ({ token, secret }) => verify(token!, secret, { algorithms: ['HS256'] }) as JWT,
   },
   callbacks: {
     jwt: async ({ token, user }) => {
-      console.log('JWT callback - user:', user) // Add logging for the user object
-
       if (user) {
         token.id = user.id
-        token.name = user.name
-        token.username = user.username
-        token.subspaceAccount = user.subspaceAccount
-        token.discordHandle = user.discordHandle
-        token.isDiscordGuildMember = user.isDiscordGuildMember
-        token.isDiscordFarmerRole = user.isDiscordFarmerRole
+        token.DIDs = user.DIDs
+        token.subspace = user.subspace
+        token.discord = user.discord
       }
-      console.log('JWT callback - token:', token) // Add logging for the token object
       return token
     },
     session: async ({ session, token }) => {
-      console.log('Session callback - token:', token) // Add logging for the token object
-      session.user = token
-      console.log('Session callback - session:', session) // Add logging for the session object
+      if (token) session.user = token
       return session
     },
   },
