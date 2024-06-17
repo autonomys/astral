1fac0420237cd2f5356e3ac2cfd3972dd01871cd
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-06-17 16:13:27 -0400

    allow to pass in the path of the page on nova explorer to reference

--- explorer/src/components/common/EvmExplorerBanner.tsx
+++ explorer/src/components/common/EvmExplorerBanner.tsx
@@ -3,7 +3,11 @@ import useDomains from 'hooks/useDomains'
 import Link from 'next/link'
 import React, { FC, useMemo } from 'react'
 
-export const EvmExplorerBanner: FC = () => {
+export const EvmExplorerBanner: FC<{ path?: string }> = ({ path }) => {
+  const href = useMemo(
+    () => (path ? EXTERNAL_ROUTES.novaExplorer + path : EXTERNAL_ROUTES.novaExplorer),
+    [path],
+  )
   return (
     <div className='w-full'>
       <div className='w-full rounded-[20px] bg-[#DDEFF1] p-5 shadow dark:border-none dark:bg-gradient-to-r dark:from-[#4141B3] dark:via-[#6B5ACF] dark:to-[#896BD2]'>
@@ -15,7 +19,7 @@ export const EvmExplorerBanner: FC = () => {
             Nova is an EVM domain. To interact with smart contracts and access more functionality,
             we recommend using our Blockscout explorer.
           </div>
-          <Link href={EXTERNAL_ROUTES.novaExplorer} target='_blank'>
+          <Link href={href} target='_blank'>
             <button className='self-start rounded-[20px] bg-white px-[33px] py-[13px] text-sm font-medium text-gray-800 hover:bg-gray-200 dark:bg-[#1E254E] dark:text-white'>
               Visit
             </button>
@@ -26,12 +30,12 @@ export const EvmExplorerBanner: FC = () => {
   )
 }
 
-export const useEvmExplorerBanner = () => {
+export const useEvmExplorerBanner = (path?: string) => {
   const { selectedChain } = useDomains()
 
   const novaExplorerBanner = useMemo(
-    () => (selectedChain?.isDomain ? <EvmExplorerBanner /> : null),
-    [selectedChain],
+    () => (selectedChain?.isDomain ? <EvmExplorerBanner path={path} /> : null),
+    [path, selectedChain],
   )
 
   return novaExplorerBanner
