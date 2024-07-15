d8ab56e0f0a6ae93ac6474f3789ea8860e5375ca
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-15 16:38:54 -0400

    add MyPositionSwitch

---
+++ explorer/src/components/common/MyPositionSwitch.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useCallback, useMemo } from 'react'
+import { useViewStates } from 'states/view'
+
+export const MyPositionSwitch: FC = () => {
+  const { myPositionOnly, setMyPositionOnly } = useViewStates()
+
+  const label = useMemo(() => (myPositionOnly ? 'Staked only' : 'All'), [myPositionOnly])
+  const onChange = useCallback(
+    () => setMyPositionOnly(!myPositionOnly),
+    [myPositionOnly, setMyPositionOnly],
+  )
+
+  return (
+    <div className='w-full'>
+      <button
+        onClick={onChange}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') onChange()
+        }}
+        className={`${
+          myPositionOnly ? 'bg-purpleAccent' : 'bg-transparent'
+        } border-color-grayDarker dark:border-color-white relative inline-flex h-6 w-11 cursor-pointer items-center rounded-full border-2`}
+        role='switch'
+        aria-checked={myPositionOnly}
+      >
+        <span
+          className={`${
+            myPositionOnly ? 'translate-x-6' : 'translate-x-1'
+          } inline-block h-4 w-4 transform rounded-full bg-white transition-transform dark:bg-purpleSoft`}
+        />
+        <span className='sr-only'>{label}</span>
+      </button>
+      <span className='ml-2 text-base dark:text-white'>{label}</span>
+    </div>
+  )
+}
