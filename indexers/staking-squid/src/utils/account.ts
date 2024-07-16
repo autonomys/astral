96801b2e4e92d1a26df43a5560089b4bf770e28b
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-16 15:00:29 -0400

    improve utils

--- indexers/staking-squid/src/utils/account.ts
+++ indexers/staking-squid/src/utils/account.ts
@@ -2,15 +2,12 @@ import { codec } from "@subsquid/ss58";
 import type { Store } from "@subsquid/typeorm-store";
 import type { ProcessorContext } from "../processor";
 
-export const getCallSigner = (
-  call: ProcessorContext<Store>["blocks"][0]["extrinsics"][0]["call"]
-): string => {
-  const raw = (call as any).origin.value.value;
-
+export const hexToAccount = (raw: string): string => {
   const signer = Buffer.from(raw.slice(2), "hex");
 
-  const account = codec(2254).encode(signer);
-  console.log("account", account);
-
-  return account;
+  return codec(2254).encode(signer);
 };
+
+export const getCallSigner = (
+  call: ProcessorContext<Store>["blocks"][0]["extrinsics"][0]["call"]
+): string => hexToAccount((call as any).origin.value.value);
--- indexers/staking-squid/src/utils/id.ts
+++
@@ -1,5 +0,0 @@
-export const operatorIdToString = (operatorId: number) =>
-  `op-${operatorId.toString().padStart(9, "0")}`;
-
-export const nominatorIdToString = (operatorId: number, account: string) =>
-  `no-${account}-op-${operatorId.toString().padStart(9, "0")}`;
