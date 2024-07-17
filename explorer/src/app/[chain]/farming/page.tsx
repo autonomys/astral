56a3b34d88d66c3f5fe808e81c753953fd353e3b
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-17 17:47:34 -0400

    change metadata image on farming page

--- explorer/src/app/[chain]/farming/page.tsx
+++ explorer/src/app/[chain]/farming/page.tsx
@@ -1,6 +1,6 @@
 import { DownloadPage } from '@/components/Farming'
 import { chains } from 'constants/chains'
-import { metadata } from 'constants/metadata'
+import { metadata, url } from 'constants/metadata'
 import { Metadata } from 'next'
 import { FC } from 'react'
 import type { ChainPageProps } from 'types/app'
@@ -8,16 +8,25 @@ import type { ChainPageProps } from 'types/app'
 export async function generateMetadata({ params: { chain } }: ChainPageProps): Promise<Metadata> {
   const chainTitle = chains.find((c) => c.urls.page === chain)?.title || 'Unknown chain'
   const title = `${metadata.title} - ${chainTitle} - Farming`
+  const images = {
+    url: url + '/images/share-farming.png',
+    secureUrl: url + 'image/png',
+    width: 900,
+    height: 600,
+    alt: title,
+  }
   return {
     ...metadata,
     title,
     openGraph: {
       ...metadata.openGraph,
       title,
+      images,
     },
     twitter: {
       ...metadata.twitter,
       title,
+      images,
     },
   }
 }
