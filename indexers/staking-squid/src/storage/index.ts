7f4d44e9ad30526a86ef6995a7c6b1f05884995a
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-23 18:45:06 -0400

    add storage helper for nominator

--- indexers/staking-squid/src/storage/index.ts
+++ indexers/staking-squid/src/storage/index.ts
@@ -1,2 +1,3 @@
 export * from "./domain";
+export * from "./nominator";
 export * from "./operator";
---
+++ indexers/staking-squid/src/storage/nominator.ts
@@ -0,0 +1,53 @@
+import type { Store } from "@subsquid/typeorm-store";
+import { randomUUID } from "crypto";
+import { Nominator, NominatorStatus, Operator } from "../model";
+import type { Ctx, CtxBlock } from "../processor";
+import { getBlockNumber } from "../utils";
+import { getOrCreateOperator } from "./operator";
+
+export const createNominator = async (
+  ctx: Ctx<Store>,
+  block: CtxBlock,
+  props: Partial<Nominator>
+): Promise<Nominator> => {
+  if (props.operator) props.operator = await getOrCreateOperator(ctx, block, 0);
+
+  const nominator = new Nominator({
+    id: randomUUID(),
+    account: "st",
+    shares: BigInt(0),
+    deposits: [],
+    withdrawals: [],
+    depositsCount: 0,
+    withdrawalsCount: 0,
+    totalDeposits: BigInt(0),
+    status: NominatorStatus.PENDING,
+    ...props,
+    createdAt: getBlockNumber(block),
+    updatedAt: getBlockNumber(block),
+  });
+
+  await ctx.store.insert(nominator);
+
+  const nominatorsCount = await ctx.store.count(Nominator);
+  ctx.log.child("nominators").info(`count: ${nominatorsCount}`);
+
+  return nominator;
+};
+
+export const getOrCreateNominator = async (
+  ctx: Ctx<Store>,
+  block: CtxBlock,
+  operator: Operator,
+  account: string
+): Promise<Nominator> => {
+  const nominator = await ctx.store.findOneBy(Nominator, { account, operator });
+
+  if (!nominator)
+    return await createNominator(ctx, block, {
+      account,
+      operator,
+    });
+
+  return nominator;
+};
