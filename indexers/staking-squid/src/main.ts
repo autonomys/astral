5316ab4bc082d9d80e2839dea9486433c81b5779
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-30 21:19:14 -0400

    increase processor profile size

--- indexers/staking-squid/src/main.ts
+++ indexers/staking-squid/src/main.ts
@@ -2,12 +2,9 @@ import { activate } from "@autonomys/auto-utils";
 import { TypeormDatabase } from "@subsquid/typeorm-store";
 import { processBlocks } from "./blocks";
 import { processor } from "./processor";
-import { save } from "./utils/cache";
 
 processor.run(new TypeormDatabase({ supportHotBlocks: true }), async (ctx) => {
   const api = await activate();
 
-  const cache = await processBlocks(ctx, api);
-
-  await save(ctx, cache);
+  await processBlocks(ctx, api);
 });
