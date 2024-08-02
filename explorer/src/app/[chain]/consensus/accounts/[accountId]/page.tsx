8f570e7f32b619853cc19dd339d106036a2ee869
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-08-02 19:36:37 -0400

    implement the change in constants

--- explorer/src/app/[chain]/consensus/accounts/[accountId]/page.tsx
+++ explorer/src/app/[chain]/consensus/accounts/[accountId]/page.tsx
@@ -1,6 +1,6 @@
 import { shortString } from '@/utils/string'
 import { Account } from 'components/Account/Account'
-import { chains } from 'constants/chains'
+import { indexers } from 'constants/indexers'
 import { metadata } from 'constants/metadata'
 import { Metadata } from 'next'
 import { FC } from 'react'
@@ -9,7 +9,7 @@ import type { AccountIdPageProps, ChainPageProps } from 'types/app'
 export async function generateMetadata({
   params: { chain, accountId },
 }: ChainPageProps & AccountIdPageProps): Promise<Metadata> {
-  const chainTitle = chains.find((c) => c.urls.page === chain)?.title || 'Unknown chain'
+  const chainTitle = indexers.find((c) => c.network === chain)?.title || 'Unknown chain'
   const title = `${metadata.title} - ${chainTitle} - Account ${accountId ? shortString(accountId) : ''}`
   return {
     ...metadata,
