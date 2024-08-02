63bb4670527f1962d9dc070e8de839e197269ab7
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-08-02 08:38:41 -0400

    use createConnection for api instead

--- indexers/staking-squid/src/main.ts
+++ indexers/staking-squid/src/main.ts
@@ -1,12 +1,15 @@
-import { activate } from "@autonomys/auto-utils";
+import { createConnection } from "@autonomys/auto-utils";
 import { TypeormDatabase } from "@subsquid/typeorm-store";
+import { assertNotNull } from "@subsquid/util-internal";
 import { processBlocks } from "./blocks";
 import { processor } from "./processor";
 
 processor.run(new TypeormDatabase({ supportHotBlocks: true }), async (ctx) => {
   console.log("Starting processor");
 
-  const api = await activate();
+  const api = await createConnection(
+    assertNotNull(process.env.RPC_CONSENSUS_HTTP, "No RPC endpoint supplied")
+  );
 
   await processBlocks(ctx, api);
 
