b0fbab61500319008917a920fe1a88500c9c3375
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-08-03 16:23:00 -0400

    improve card and autonomys symbol

--- explorer/src/components/common/Card.tsx
+++ explorer/src/components/common/Card.tsx
@@ -1,14 +1,17 @@
+// file: explorer/src/components/common/Card.tsx
+
 import { FC } from 'react'
 
 type CardProps = {
   children: React.ReactNode
   className?: string
+  width?: string
 }
 
-export const Card: FC<CardProps> = ({ children, className }) => {
+export const Card: FC<CardProps> = ({ children, className, width = 'w-full' }) => {
   return (
-    <div className={`flex w-full flex-col align-middle ${className}`}>
-      <div className='mt-2 flex w-full flex-col sm:mt-0'>
+    <div className={`flex  ${width} flex-col align-middle ${className}`}>
+      <div className={'mt-2 flex w-full flex-col sm:mt-0'}>
         <div className="w-full rounded-[20px] bg-white p-6 font-['Montserrat'] dark:border-none dark:bg-gradient-to-r dark:from-gradientTwilight dark:via-gradientDusk dark:to-gradientSunset">
           {children}
         </div>
