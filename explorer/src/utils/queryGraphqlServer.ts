15fd4380ae622ccc034ee2298d2dd8c0556b7104
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-08-28 13:24:32 -0400

    use next callback urk to retrieve network instead

--- explorer/src/utils/queryGraphqlServer.ts
+++ explorer/src/utils/queryGraphqlServer.ts
@@ -5,11 +5,17 @@ export const queryGraphqlServer = async (query: string, variables: object) => {
   try {
     // Get the selected chain from the cookies
     const { get } = cookies()
-    const selectedNetwork = get('selected-network')
-    if (!selectedNetwork) throw new Error('No selected network')
+    const callbackUrlCookie = get('next-auth.callback-url')
+    if (!callbackUrlCookie) throw new Error('No callback URL cookie')
+
+    // Extract the network ID from the callback URL
+    const callbackUrl = decodeURIComponent(callbackUrlCookie.value)
+    const url = new URL(callbackUrl)
+    const networkId = url.pathname.split('/')[1] // The network ID is the second part of the path
+    if (!networkId) throw new Error('No network ID found in callback URL')
 
     // Find the selected chain api
-    const api = indexers.find((indexer) => indexer.network === selectedNetwork.value)
+    const api = indexers.find((indexer) => indexer.network === networkId)
     if (!api) throw new Error('No selected chain api')
 
     // Fetch the data from the api
