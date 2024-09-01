b779803af98a0ca57c65e4ca0da1408c29d212a3
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-01 19:08:11 -0400

    add dummy FAQ components

---
+++ explorer/src/components/TestnetRewards/FAQ.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { FC, useState } from 'react'
+
+export const FAQ: FC = () => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null)
+
+  const toggleFAQ = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index)
+  }
+
+  return (
+    <div className='mt-8 w-full max-w-xl'>
+      <h2 className='text-center text-xl font-bold text-gray-900 dark:text-white'>FAQ</h2>
+      <div className='mt-4'>
+        {[
+          'How will the rewards be converted to ATC?',
+          'When can I withdraw the ATC?',
+          'How else can I use ATC?',
+        ].map((question, index) => (
+          <div key={index} className='m-4'>
+            <button
+              className='w-full rounded-lg bg-white p-8 text-left shadow-md dark:bg-gray-800'
+              onClick={() => toggleFAQ(index)}
+            >
+              <span className='font-semibold'>
+                {index + 1}. {question}
+              </span>
+              <span className='float-right'>{openIndex === index ? '▲' : '▼'}</span>
+            </button>
+            {openIndex === index && (
+              <div className='mt-2 rounded-lg bg-gray-100 p-4 dark:bg-gray-700'>
+                <p className='text-gray-700 dark:text-white'>
+                  {/* Add the answer content here */}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+                  Praesent libero. Sed cursus ante dapibus diam.
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
