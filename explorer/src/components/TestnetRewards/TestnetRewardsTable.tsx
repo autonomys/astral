50fea6b9eea7083d6ab9b6845b9f58850ba82973
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-04 07:44:50 -0400

    add some of the db logic (wip)

--- explorer/src/components/TestnetRewards/TestnetRewardsTable.tsx
+++ explorer/src/components/TestnetRewards/TestnetRewardsTable.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { useSquidQuery } from '@/hooks/useSquidQuery'
+import { bigNumberToFormattedString } from '@/utils/number'
+import { sendGAEvent } from '@next/third-parties/google'
 import {
   AriesStressTestIcon,
   Gemini3fTestnetIcon,
@@ -9,88 +12,143 @@ import {
   StakeWarsIIcon,
   StakeWarsIIIcon,
 } from 'components/icons/TestnetIcon'
-import { FC } from 'react'
+import {
+  AccountsPerCampaignListQuery,
+  AccountsPerCampaignListQueryVariables,
+  Order_By as OrderBy,
+} from 'gql/types/testnetRewards'
+import useWallet from 'hooks/useWallet'
+import { useWindowFocus } from 'hooks/useWindowFocus'
+import { FC, useEffect, useMemo } from 'react'
+import { useInView } from 'react-intersection-observer'
+import { QUERY_ACCOUNTS_PER_CAMPAIGN_LIST } from './testnetRewards.query'
 
 export const TestnetRewardsTable: FC = () => {
-  const rewards = [
-    {
-      icon: <AriesStressTestIcon />,
-      testnet: 'Aries Stress Test',
-      earningsPercent: '0.00001',
-      earningsTSSC: '100',
-      earningsATC: '0.0000029',
-      rank: '#560',
-      dateRange: '01.05.2021 - 01.06.2021',
-    },
-    {
-      icon: <GeminiIIncentivizedTestnetIcon />,
-      testnet: 'Gemini I Incentivized Testnet',
-      earningsPercent: '0.00001',
-      earningsTSSC: '100',
-      earningsATC: '0.0000029',
-      rank: '#23560',
-      dateRange: '01.04.2022 - 25.07.2022',
-    },
-    {
-      icon: <GeminiIITestnetIcon />,
-      testnet: 'Gemini II Testnet',
-      earningsPercent: '0.00001',
-      earningsTSSC: '100',
-      earningsATC: '0.0000029',
-      rank: '#23560',
-      dateRange: '01.04.2022 - 25.07.2022',
-    },
-    {
-      icon: <Gemini3fTestnetIcon />,
-      testnet: 'Gemini 3f Testnet',
-      earningsPercent: '0.00001',
-      earningsTSSC: '100',
-      earningsATC: '0.0000029',
-      rank: '#23560',
-      dateRange: '01.04.2022 - 25.07.2022',
-    },
-    {
-      icon: <Gemini3hTestnetIcon />,
-      testnet: 'Gemini 3h Testnet',
-      earningsPercent: '0.00001',
-      earningsTSSC: '100',
-      earningsATC: '0.0000029',
-      rank: '#23560',
-      dateRange: '01.04.2022 - 25.07.2022',
-    },
-    {
-      icon: <StakeWarsIIcon />,
-      testnet: 'Stake Wars I',
-      earningsPercent: '0.00001',
-      earningsTSSC: '100',
-      earningsATC: '0.0000029',
-      rank: '#23560',
-      dateRange: '01.04.2022 - 25.07.2022',
-    },
-    {
-      icon: <StakeWarsIIIcon />,
-      testnet: 'Stake Wars II',
-      earningsPercent: '0.00001',
-      earningsTSSC: '100',
-      earningsATC: '0.0000029',
-      rank: '#23560',
-      dateRange: '01.04.2022 - 25.07.2022',
-    },
-  ]
+  const { ref, inView } = useInView()
+  const { subspaceAccount } = useWallet()
+  const inFocus = useWindowFocus()
 
-  const totalEarningsPercent = rewards
-    .reduce((acc, reward) => acc + parseFloat(reward.earningsPercent), 0)
-    .toFixed(5)
-  const totalEarningsTSSC = rewards.reduce(
-    (acc, reward) => acc + parseFloat(reward.earningsTSSC),
-    0,
+  const where = useMemo(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const conditions: Record<string, any> = {}
+
+    if (subspaceAccount) {
+      // eslint-disable-next-line camelcase
+      conditions.account_id = { _eq: subspaceAccount }
+    }
+
+    return conditions
+  }, [subspaceAccount])
+
+  const variables = useMemo(
+    () =>
+      ({
+        limit: 12,
+        offset: undefined,
+        // eslint-disable-next-line camelcase
+        orderBy: { campaign_id: OrderBy.Asc },
+        where,
+      }) as AccountsPerCampaignListQueryVariables,
+    [where],
   )
-  const totalEarningsATC = rewards
-    .reduce((acc, reward) => acc + parseFloat(reward.earningsATC), 0)
-    .toFixed(7)
+
+  const { loading, data, setIsVisible } = useSquidQuery<
+    AccountsPerCampaignListQuery,
+    AccountsPerCampaignListQueryVariables
+  >(QUERY_ACCOUNTS_PER_CAMPAIGN_LIST, {
+    variables,
+    skip: !inFocus,
+    pollInterval: 6000,
+    context: { clientName: 'testnetRewards' },
+  })
+
+  const campaigns = useMemo(
+    () => ({
+      aries: {
+        testnet: 'Aries Stress Test',
+        icon: <AriesStressTestIcon />,
+        dateRange: '01.05.2021 - 01.06.2021',
+      },
+      'gemini-1': {
+        testnet: 'Gemini 1',
+        icon: <GeminiIIncentivizedTestnetIcon />,
+        dateRange: '01.04.2022 - 25.07.2022',
+      },
+      'gemini-2.1': {
+        testnet: 'Gemini 2.1',
+        icon: <GeminiIIncentivizedTestnetIcon />,
+        dateRange: '01.04.2022 - 25.07.2022',
+      },
+      'gemini-2.2': {
+        testnet: 'Gemini 2.2',
+        icon: <GeminiIITestnetIcon />,
+        dateRange: '01.04.2022 - 25.07.2022',
+      },
+      'gemini-3f': {
+        testnet: 'Gemini 3f',
+        icon: <Gemini3fTestnetIcon />,
+        dateRange: '01.04.2022 - 25.07.2022',
+      },
+      'gemini-3g': {
+        testnet: 'Gemini 3g',
+        icon: <Gemini3fTestnetIcon />,
+        dateRange: '01.04.2022 - 25.07.2022',
+      },
+      'gemini-3h': {
+        testnet: 'Gemini 3h',
+        icon: <Gemini3hTestnetIcon />,
+        dateRange: '01.04.2022 - 25.07.2022',
+      },
+      'stake-wars-i': {
+        testnet: 'Stake Wars I',
+        icon: <StakeWarsIIcon />,
+        dateRange: '01.04.2022 - 25.07.2022',
+      },
+      'stake-wars-ii': {
+        testnet: 'Stake Wars II',
+        icon: <StakeWarsIIIcon />,
+        dateRange: '01.04.2022 - 25.07.2022',
+      },
+    }),
+    [],
+  )
+
+  const rewards = useMemo(() => {
+    if (loading || !data) return []
+
+    return data.account_per_campaign.map((item) => ({
+      ...campaigns[item.campaign_id as keyof typeof campaigns],
+      earningsPercent: item.total_earnings_percentage_testnet_token,
+      earningsTSSC: bigNumberToFormattedString(item.total_earnings_amount_testnet_token),
+      earningsATC: bigNumberToFormattedString(item.total_earnings_amount_atc_token),
+      rank: item.rank,
+    }))
+  }, [data, loading, campaigns])
+
+  const totalEarningsPercent = '0.1'
+  const totalEarningsTSSC = data
+    ? data.account_per_campaign.reduce(
+        (acc, reward) => acc + parseFloat(reward.total_earnings_amount_testnet_token),
+        0,
+      )
+    : 0
+  const totalEarningsATC = data
+    ? data.account_per_campaign.reduce(
+        (acc, reward) => acc + parseFloat(reward.total_earnings_amount_atc_token),
+        0,
+      )
+    : 0
+
+  useEffect(() => {
+    setIsVisible(inView)
+  }, [inView, setIsVisible])
+
+  useEffect(() => {
+    sendGAEvent('event', 'check_testnet_rewards', { value: subspaceAccount })
+  }, [subspaceAccount])
 
   return (
-    <div className='max-w-8xl mt-8 w-full'>
+    <div className='max-w-8xl mt-8 w-full' ref={ref}>
       <div className='mt-4 rounded-lg bg-white p-4 shadow-md dark:bg-gray-800'>
         <h3 className='text-center text-lg font-bold text-gray-400 dark:text-white'>
           Your rewards at Autonomys (Subspace Network) Testnets
@@ -172,7 +230,7 @@ export const TestnetRewardsTable: FC = () => {
               {totalEarningsPercent} out of 0.1%
             </div>
             <div className='text-sm text-gray-500 dark:text-gray-300'>
-              {totalEarningsTSSC} out of 10000 tSSC
+              {bigNumberToFormattedString(totalEarningsTSSC)} out of 10000 tSSC
             </div>
             <div className='text-sm text-gray-500 dark:text-gray-300'>
               {totalEarningsATC} out of ......
