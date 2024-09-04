0329cd7740c7010b4e073d93d1f12d6fa2a3340c
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-04 07:44:26 -0400

    add ga events

--- explorer/src/components/TestnetRewards/index.tsx
+++ explorer/src/components/TestnetRewards/index.tsx
@@ -1,16 +1,24 @@
 'use client'
 
-import { FC } from 'react'
+import { sendGAEvent } from '@next/third-parties/google'
+import useWallet from 'hooks/useWallet'
+import { FC, useEffect } from 'react'
 import { FAQ } from './FAQ'
 import { RewardHistory } from './RewardHistory'
 import { TestnetRewardsTable } from './TestnetRewardsTable'
 
 export const TestnetRewardsPage: FC = () => {
+  const { subspaceAccount } = useWallet()
+
+  useEffect(() => {
+    sendGAEvent('event', 'visit_testnet_rewards_page', { value: 'index' })
+  }, [])
+
   return (
     <div className='flex min-h-screen w-full flex-col items-center space-y-4 p-4'>
       <RewardHistory />
       <FAQ />
-      <TestnetRewardsTable />
+      {subspaceAccount && <TestnetRewardsTable />}
     </div>
   )
 }
