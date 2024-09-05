29e91c725c0985a2a1e390994e5ffeb5d5198042
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-05 08:23:27 -0400

    improve % precision

--- indexers/testnet-rewards-squid/src/utils/percentage.ts
+++ indexers/testnet-rewards-squid/src/utils/percentage.ts
@@ -1,7 +1,14 @@
 import { Cache } from "./cache";
 
 export const calculatePercentage = (cache: Cache): Cache => {
+  const precision = BigInt(10) ** BigInt(16);
   const campaigns = cache.campaigns.values();
+  const accounts = cache.accounts.values();
+
+  const totalEarnings = cache.totalEarnings.get("0");
+  const totalEarningsAmountTestnetToken = totalEarnings
+    ? totalEarnings.totalEarningsAmountTestnetToken
+    : BigInt(0);
 
   for (const campaign of campaigns) {
     const totalEarningsAmountTestnetToken =
@@ -13,7 +20,9 @@ export const calculatePercentage = (cache: Cache): Cache => {
 
     for (const accountPerCampaign of accountsForThisCampaign) {
       const percentage =
-        (accountPerCampaign.totalEarningsAmountTestnetToken * BigInt(100)) /
+        (accountPerCampaign.totalEarningsAmountTestnetToken *
+          precision *
+          BigInt(100)) /
         totalEarningsAmountTestnetToken;
 
       accountPerCampaign.totalEarningsPercentageTestnetToken =
@@ -21,6 +30,16 @@ export const calculatePercentage = (cache: Cache): Cache => {
 
       cache.accountPerCampaigns.set(accountPerCampaign.id, accountPerCampaign);
     }
+  }
+
+  for (const account of accounts) {
+    const percentage =
+      (account.totalEarningsAmountTestnetToken * precision * BigInt(100)) /
+      totalEarningsAmountTestnetToken;
+
+    account.totalEarningsPercentageTestnetToken = percentage.toString();
+
+    cache.accounts.set(account.id, account);
   }
 
   return cache;
