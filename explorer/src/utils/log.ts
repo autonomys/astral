b5b3bcbcd5a3d08d917a410c6ad64f660944c640
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-05 15:10:00 -0400

    add logTx utils function

--- explorer/src/utils/log.ts
+++ explorer/src/utils/log.ts
@@ -1,14 +1,17 @@
-const log = async (type: string, pathname: string, error: Error) =>
+const log = async (type: string, pathname: string, error: Error | string) =>
   await fetch(`/api/log/${type}/${pathname}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      message: error.message,
-      stack: error.stack,
+      message: typeof error === 'string' ? error : error.message,
+      stack: typeof error === 'string' ? null : error.stack,
     }),
   })
 
-export const logError = async (pathname: string, error: Error) =>
+export const logError = async (pathname: string, error: Error | string) =>
   await log('error', pathname, error)
+
+export const logTx = async (pathname: string, txHash: string, call: string) =>
+  await log('tx', pathname, 'tx: ' + txHash + ' call: ' + call)
