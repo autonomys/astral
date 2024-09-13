d6535d97be05fd031076532403385b4b67574fb6
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-13 11:09:32 -0400

    improve multi-indexers setup

--- indexers/consensus/src/mappings/mappingHandlers.ts
+++ indexers/consensus/src/mappings/mappingHandlers.ts
@@ -1,22 +1,51 @@
-import {
-  SubstrateBlock,
-  SubstrateEvent,
-  SubstrateExtrinsic,
-} from "@subql/types";
+import { balance } from "@autonomys/auto-consensus";
+// import { ApiAtBlockHash } from "@autonomys/auto-utils";
+// import { Struct, u64 } from "@polkadot/types";
+// import { AccountId32 } from "@polkadot/types/interfaces";
+import { SubstrateBlock } from "@subql/types";
+// import { HexSink } from "@subsquid/scale-codec";
+// import { xxhash128 } from "@subsquid/util-xxhash";
+import { solutionRanges } from "../typegens/subspace/storage";
+// import { Block as BlockSQD } from "../typegens/support";
+// import { digest } from "../typegens/system/storage";
+// import { DigestItem_PreRuntime } from "../typegens/v0";
 import { Account, Block, Event, Extrinsic } from "../types";
 
+const PIECE_SIZE = BigInt(1048576);
+const MAX_PIECES_IN_SECTOR = BigInt(1000);
+
+const ENDPOINT = "wss://rpc-squids.gemini-3h.subspace.network/ws";
+
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
+  const test = await balance(
+    api as any,
+    "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+  );
+  logger.info(`test: ${test} ${test.free.toString()}`);
   // Deconstruct the block object
+  const { block: blockHeader, timestamp, specVersion, events } = block;
   const {
-    block: {
-      header: { number, parentHash, stateRoot, extrinsicsRoot },
-      hash,
-      extrinsics,
-    },
-    timestamp,
-    specVersion,
-    events,
-  } = block;
+    header: { number, parentHash, stateRoot, extrinsicsRoot },
+    hash,
+    extrinsics,
+  } = blockHeader;
+
+  logger.info(`Connecting to ${ENDPOINT}`);
+
+  logger.info(`Block Hash: ${hash.toString()}`);
+
+  // const [solutionRang, blockchainSize, owner] = await Promise.all([
+  // solutionRanges.v0.get(blockHeader as any),
+  // getHistorySize(blockHeader.hash.toString(), api as any),
+  // getBlockAuthor(blockHeader as any, api as any),
+  // ]);
+
+  const solutionRang = await solutionRanges.v0.get(blockHeader as any);
+  const spacePledged = solutionRang
+    ? calcSpacePledged(solutionRang.current)
+    : calcSpacePledged(
+        solutionRanges.v0.getDefault(blockHeader as any).current
+      );
 
   logger.info(`Block number: ${number.toString()}`);
   logger.info(`Timestamp: ${timestamp}`);
@@ -29,8 +58,8 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
   logger.info(`Events count: ${events.length}`);
 
   // To-Do
-  const spacePledged = BigInt(0);
-  const blockchainSize = BigInt(0);
+  // const spacePledged = BigInt(0);
+  // const blockchainSize = BigInt(0);
   logger.info(`spacePledged: ${spacePledged.toString()}`);
   logger.info(`blockchainSize: ${blockchainSize.toString()}`);
 
@@ -105,3 +134,96 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
   await blockRecord.save();
   logger.info("Block record saved successfully");
 }
+
+export const solutionRangeToSectors = (solutionRange: bigint): bigint => {
+  const MAX_U64 = BigInt(2) ** BigInt(64) - BigInt(1);
+  const SLOT_PROBABILITY = [BigInt(1), BigInt(6)];
+  const RECORD_NUM_CHUNKS = BigInt(32768);
+  const RECORD_NUM_S_BUCKETS = BigInt(65536);
+
+  const sectors =
+    ((MAX_U64 / SLOT_PROBABILITY[1]) * SLOT_PROBABILITY[0]) /
+    ((MAX_PIECES_IN_SECTOR * RECORD_NUM_CHUNKS) / RECORD_NUM_S_BUCKETS);
+
+  // Take solution range into account
+  return sectors / solutionRange;
+};
+
+export const calcSpacePledged = (solutionRange: bigint): bigint => {
+  const sectors = solutionRangeToSectors(solutionRange);
+
+  return sectors * MAX_PIECES_IN_SECTOR * PIECE_SIZE;
+};
+
+// export const calcHistorySize = (segmentsCount: number): bigint => {
+//   const PIECES_IN_SEGMENT = BigInt(256);
+//   const segmentsCountBigInt = BigInt(segmentsCount);
+
+//   return PIECE_SIZE * PIECES_IN_SEGMENT * segmentsCountBigInt;
+// };
+
+// interface Solution extends Struct {
+//   readonly public_key: AccountId32;
+//   readonly reward_address: AccountId32;
+// }
+
+// export interface SubPreDigest extends Struct {
+//   readonly slot: u64;
+//   readonly solution: Solution;
+// }
+
+// export const getBlockAuthor = async (
+//   blockHeader: BlockSQD,
+//   api: ApiAtBlockHash
+// ) => {
+//   if (blockHeader.height === 0) return; // genesis block does not have logs
+//   const storage =
+//     (await digest.v0.get(blockHeader)) ?? digest.v0.getDefault(blockHeader);
+//   const preRuntimeRaw = storage.logs.find(
+//     (digestItem) => digestItem.__kind === "PreRuntime"
+//   ) as DigestItem_PreRuntime;
+//   if (!preRuntimeRaw) return;
+
+//   const subPreDigest = api.registry.createType(
+//     "SubPreDigest",
+//     preRuntimeRaw.value[1]
+//   );
+//   return (
+//     subPreDigest as unknown as SubPreDigest
+//   ).solution.reward_address.toString();
+// };
+
+// const getNameHash = (name: string): string => {
+//   const digest = xxhash128().update(name).digest();
+//   const sink = new HexSink();
+//   sink.u128(digest);
+//   const hash = sink.toHex();
+//   return hash;
+// };
+
+// export const getStorageHash = (prefix: string, name: string) => {
+//   return getNameHash(prefix) + getNameHash(name).slice(2);
+// };
+
+// export const getHistorySize = async (
+//   blockHeaderHash: string,
+//   api: ApiAtBlockHash
+// ) => {
+//   const storageHash = getStorageHash("Subspace", "SegmentCommitment");
+
+//   const totalSizeBytes = await api.query.state.getStorageSizeAt(
+//     storageHash,
+//     blockHeaderHash
+//   );
+//   const totalSize = BigInt(totalSizeBytes.toString());
+//   if (totalSize === BigInt(0)) return BigInt(0);
+
+//   const keys = await api.query.state.getChildKeys(storageHash, blockHeaderHash);
+
+//   const keySize = await api.query.state.getStorageSizeAt(keys, blockHeaderHash);
+//   const keySizeBigInt = BigInt(keySize.toString());
+
+//   const segmentsCount = totalSize / keySizeBigInt;
+
+//   return calcHistorySize(Number(segmentsCount));
+// };
--- indexers/db/docker-entrypoint-initdb.d/init-db.sql
+++ indexers/db/docker-entrypoint-initdb.d/init-db.sql
@@ -2,3 +2,15 @@ CREATE DATABASE accounts;
 CREATE DATABASE consensus;
 CREATE DATABASE leaderboard;
 CREATE DATABASE staking;
+
+\c accounts
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+
+\c consensus
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+
+\c leaderboard
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+
+\c staking
+CREATE EXTENSION IF NOT EXISTS btree_gist;
