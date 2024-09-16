b68d83dee0ef1a2500d345e763e96501a5c31e6f
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-16 11:17:28 -0400

    improve project building flow

--- indexers/gemini-3h/staking/project.ts
+++ indexers/gemini-3h/staking/project.ts
@@ -3,17 +3,11 @@ import {
   SubstrateHandlerKind,
   SubstrateProject,
 } from "@subql/types";
-
 import * as dotenv from "dotenv";
 import path from "path";
 
-const mode = process.env.NODE_ENV || "production";
-
 // Load the appropriate .env file
-const dotenvPath = path.resolve(
-  __dirname,
-  `.env${mode !== "production" ? `.${mode}` : ""}`
-);
+const dotenvPath = path.resolve(__dirname, `../../.env`);
 dotenv.config({ path: dotenvPath });
 
 // Can expand the Datasource processor types via the genreic param
@@ -37,7 +31,7 @@ const project: SubstrateProject = {
   },
   network: {
     /* The genesis hash of the network (hash of block 0) */
-    chainId: process.env.CHAIN_ID!,
+    chainId: process.env.GEMINI_3H_CHAIN_ID!,
     /**
      * These endpoint(s) should be public non-pruned archive node
      * We recommend providing more than one endpoint for improved reliability, performance, and uptime
@@ -46,7 +40,7 @@ const project: SubstrateProject = {
      * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
      * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
-    endpoint: process.env.ENDPOINT!?.split(",") as string[] | string,
+    endpoint: process.env.GEMINI_3H_RPC!?.split(",") as string[] | string,
     // dictionary: process.env.DICTIONARY!,
   },
   dataSources: [
