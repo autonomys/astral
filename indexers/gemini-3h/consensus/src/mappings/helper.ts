dcb6bc17a3f9ba3e62fd2d07325ec4db2730c779
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-20 17:08:39 -0400

    improve consensus sub query utils and helper mapping

---
+++ indexers/gemini-3h/consensus/src/mappings/helper.ts
@@ -0,0 +1,28 @@
+import { SubstrateBlock } from "@subql/types";
+import { DEFAULT_ACCOUNT_ID } from "../constants";
+import { decodeLog } from "./utils";
+
+export const getBlockAuthor = (block: SubstrateBlock): string => {
+  const { digest } = block.block.header;
+  const preRuntimeRaw = digest.logs.find((digestI) => digestI.isPreRuntime);
+  if (preRuntimeRaw) {
+    const value = decodeLog(preRuntimeRaw.asPreRuntime);
+    if (value) {
+      api.registry.register({
+        Solution: {
+          public_key: "AccountId32",
+          reward_address: "AccountId32",
+        },
+        SubPreDigest: {
+          slot: "u64",
+          solution: "Solution",
+        },
+      });
+      const type = api.registry.createType("SubPreDigest", value.data);
+      const publicKey = (type.toPrimitive() as any).solution.public_key;
+      const rewardAddress = (type.toPrimitive() as any).solution.reward_address;
+      return rewardAddress;
+    }
+  }
+  return DEFAULT_ACCOUNT_ID;
+};
