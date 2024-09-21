ebf085a78da234cc53388d4ffdd2be65ee4750a8
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-21 11:45:23 -0400

    add autonomys dictionary

--- indexers/db/docker-entrypoint-initdb.d/init-db.sql
+++ indexers/db/docker-entrypoint-initdb.d/init-db.sql
@@ -1,7 +1,11 @@
+CREATE DATABASE gemini_3h;
 CREATE DATABASE gemini_3h_accounts;
 CREATE DATABASE gemini_3h_consensus;
 CREATE DATABASE gemini_3h_leaderboard;
 CREATE DATABASE gemini_3h_staking;
+
+\c gemini_3h
+CREATE EXTENSION IF NOT EXISTS btree_gist;
 
 \c gemini_3h_accounts
 CREATE EXTENSION IF NOT EXISTS btree_gist;
