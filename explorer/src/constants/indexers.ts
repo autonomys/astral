a391ae3ca0e4e6a26b73801a62618f4bcc6db3da
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-10-05 17:36:34 -0400

    change endpoint and indexers types

--- explorer/src/constants/indexers.ts
+++ explorer/src/constants/indexers.ts
@@ -3,31 +3,19 @@ import { NetworkId } from '@autonomys/auto-utils'
 export interface Indexer {
   title: string
   network: NetworkId
-  squids: {
-    old: string
-    accounts?: string
-    leaderboard?: string
-    staking?: string
-  }
+  indexer: string
 }
 
 export const indexers: Indexer[] = [
   {
     title: 'Gemini 3h',
     network: NetworkId.GEMINI_3H,
-    squids: {
-      old: 'https://squid.gemini-3h.subspace.network/graphql',
-      accounts: 'https://autonomys-labs.squids.live/accounts-squid/addons/hasura/v1/graphql',
-      leaderboard: 'https://autonomys-labs.squids.live/leaderboard-squid/addons/hasura/v1/graphql',
-      staking: 'https://autonomys-labs.squids.live/staking-squid/addons/hasura/v1/graphql',
-    },
+    indexer: 'http://localhost:8080/v1/graphql',
   },
   {
     title: 'Localhost',
     network: NetworkId.LOCALHOST,
-    squids: {
-      old: 'http://localhost:4349/graphql',
-    },
+    indexer: 'http://localhost:8080/v1/graphql',
   },
 ]
 
