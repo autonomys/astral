16f5529d58c3e3c3b648fa3723f354d5f6041972
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-10-10 09:23:54 -0400

    add open graph image for testnet rewards page

--- explorer/src/app/[chain]/testnet-rewards/page.tsx
+++ explorer/src/app/[chain]/testnet-rewards/page.tsx
@@ -1,6 +1,6 @@
 import { TestnetRewardsPage } from '@/components/TestnetRewards'
 import { indexers } from 'constants/indexers'
-import { metadata } from 'constants/metadata'
+import { metadata, url } from 'constants/metadata'
 import { Metadata } from 'next'
 import { FC } from 'react'
 import type { ChainPageProps } from 'types/app'
@@ -8,16 +8,25 @@ import type { ChainPageProps } from 'types/app'
 export async function generateMetadata({ params: { chain } }: ChainPageProps): Promise<Metadata> {
   const chainTitle = indexers.find((c) => c.network === chain)?.title || 'Unknown chain'
   const title = `${metadata.title} - ${chainTitle} - Testnet Rewards`
+  const images = {
+    url: url + '/images/share-testnet-rewards.png',
+    secureUrl: url + 'image/png',
+    width: 900,
+    height: 600,
+    alt: title,
+  }
   return {
     ...metadata,
     title,
     openGraph: {
       ...metadata.openGraph,
       title,
+      images,
     },
     twitter: {
       ...metadata.twitter,
       title,
+      images,
     },
   }
 }
