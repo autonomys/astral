dc8b4248c91fa4eacbc5de4299db0dabd2676510
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-10-11 16:07:12 -0400

    remove redundant functions

--- indexers/gemini-3h/consensus/src/mappings/utils.ts
+++ indexers/gemini-3h/consensus/src/mappings/utils.ts
@@ -1,8 +1,5 @@
 import { SubstrateBlock } from "@subql/types";
 
-const PIECE_SIZE = BigInt(1048576);
-const MAX_PIECES_IN_SECTOR = BigInt(1000);
-
 export const stringify = (value: any) =>
   JSON.stringify(value, (_, value) =>
     typeof value === "bigint" ? value.toString() : value
@@ -33,33 +30,6 @@ export const decodeLog = (value: null | Uint8Array | Uint8Array[]) => {
   }
 
   return { data: value };
-};
-
-export const solutionRangeToSectors = (solutionRange: bigint): bigint => {
-  logger.info(
-    `solutionRangeToSectors.solutionRange ${solutionRange.toString()}`
-  );
-  const MAX_U64 = BigInt(2 ** 64 - 1);
-  const SLOT_PROBABILITY = [BigInt(1), BigInt(6)];
-  const RECORD_NUM_CHUNKS = BigInt(32768);
-  const RECORD_NUM_S_BUCKETS = BigInt(65536);
-
-  const sectors =
-    ((MAX_U64 / SLOT_PROBABILITY[1]) * SLOT_PROBABILITY[0]) /
-    ((MAX_PIECES_IN_SECTOR * RECORD_NUM_CHUNKS) / RECORD_NUM_S_BUCKETS);
-
-  // Take solution range into account
-  return sectors / solutionRange;
-};
-
-export const calcSpacePledged = (solutionRange: bigint): bigint =>
-  solutionRangeToSectors(solutionRange) * MAX_PIECES_IN_SECTOR * PIECE_SIZE;
-
-export const calcHistorySize = (segmentsCount: number): bigint => {
-  const PIECES_IN_SEGMENT = BigInt(256);
-  const segmentsCountBigInt = BigInt(segmentsCount);
-
-  return PIECE_SIZE * PIECES_IN_SEGMENT * segmentsCountBigInt;
 };
 
 export const capitalizeFirstLetter = (str: string) =>
