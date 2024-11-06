b03ecd755ecc9383a595d6ce26563621d99dbd50
Marc-Aurele Besner <marcaurele.desk@gmail.com>
2024-11-06 11:17:42 -0500

    use the default db instead

--- indexers/db/docker-entrypoint-initdb.d/init-db.sql
+++ indexers/db/docker-entrypoint-initdb.d/init-db.sql
@@ -1,15 +1,2 @@
-CREATE DATABASE taurus;
-
-\c taurus
-CREATE EXTENSION IF NOT EXISTS btree_gist;
-
-CREATE DATABASE gemini_3h;
-
-\c gemini_3h
-CREATE EXTENSION IF NOT EXISTS btree_gist;
-
--- Additional databases for testnet rewards
-CREATE DATABASE gemini_3g_testnet_rewards;
-
-\c gemini_3g_testnet_rewards
+\c postgres
 CREATE EXTENSION IF NOT EXISTS btree_gist;
