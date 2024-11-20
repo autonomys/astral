113d2f8cbf13233ec2fb933cc6d383a1d26056f7
Marc-Aurele Besner <marcaurele.desk@gmail.com>
2024-11-20 10:38:20 -0500

    apply changes to db helper function and add hexToUint8Array in utils

--- indexers/mainnet/consensus/src/mappings/utils.ts
+++ indexers/mainnet/consensus/src/mappings/utils.ts
@@ -48,3 +48,11 @@ export const getSortId = (
   const str2 = indexInBlock.toString().padStart(totalLength, "0");
   return `${str1}-${str2}`;
 };
+
+export const hexToUint8Array = (hex: string): Uint8Array => {
+  if (hex.length % 2 !== 0)
+    throw new Error("Hex string must have an even length");
+  return new Uint8Array(
+    hex.match(/.{1,2}/g)?.map((byte) => parseInt(byte, 16)) || []
+  );
+};
