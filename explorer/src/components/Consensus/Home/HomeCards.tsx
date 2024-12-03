d844776f8de0d573ff8533167059a1f7480a6508
Marc-Aurele Besner <marcaurele.desk@gmail.com>
2024-12-03 11:50:26 -0500

    add node count

--- explorer/src/components/Consensus/Home/HomeCards.tsx
+++ explorer/src/components/Consensus/Home/HomeCards.tsx
@@ -1,21 +1,19 @@
-import { ArchivedHistoryIcon, BlockIcon, PieChartIcon } from 'components/icons' // , WalletIcon, DocIcon
+import { ArchivedHistoryIcon, BlockIcon, PieChartIcon, WalletIcon } from 'components/icons'
 import { FC, useMemo } from 'react'
 import { HomeInfoCard } from './HomeInfoCard'
 
 type Props = {
   blocksCount?: string
   spacePledged?: string
-  spacePledgedBinary?: string
+  nodeCount?: string
   historySize?: string
-  historySizeBinary?: string
 }
 
 export const HomeCards: FC<Props> = ({
   blocksCount = '0',
   spacePledged = '0',
-  spacePledgedBinary = '0',
   historySize = '0',
-  historySizeBinary = '0',
+  nodeCount = '0',
 }) => {
   const listOfCards = useMemo(
     () => [
@@ -27,17 +25,11 @@ export const HomeCards: FC<Props> = ({
           'dark:bg-gradient-to-b dark:from-purpleLighterAccent dark:via-pastelPurple dark:to-pastelBlue',
       },
       {
-        title: 'Total Space Pledged',
-        icon: <PieChartIcon />,
-        value: spacePledgedBinary,
+        title: 'Total Nodes',
+        icon: <WalletIcon />,
+        value: nodeCount,
         darkBgClass:
           'dark:bg-gradient-to-b dark:from-purpleLighterAccent dark:via-purpleShade dark:to-pastelPurple',
-      },
-      {
-        title: 'Archived History Size',
-        icon: <ArchivedHistoryIcon />,
-        value: historySizeBinary,
-        darkBgClass: 'dark:bg-gradient-to-b dark:from-pastelBlue dark:to-pastelPink',
       },
       {
         title: 'Total Space Pledged',
@@ -53,7 +45,7 @@ export const HomeCards: FC<Props> = ({
         darkBgClass: 'dark:bg-gradient-to-b dark:from-pastelBlue dark:to-pastelPink',
       },
     ],
-    [blocksCount, historySize, spacePledged, spacePledgedBinary, historySizeBinary],
+    [blocksCount, nodeCount, spacePledged, historySize],
   )
 
   return (
--- explorer/src/components/Consensus/Home/HomeChainInfo.tsx
+++ explorer/src/components/Consensus/Home/HomeChainInfo.tsx
@@ -1,22 +1,60 @@
 import { Spinner } from '@/components/common/Spinner'
-import { formatSpaceToBinary, formatSpaceToDecimal } from '@autonomys/auto-consensus'
+import { formatSpaceToDecimal } from '@autonomys/auto-consensus'
 import { useHomeCardsQueryQuery } from 'gql/graphql'
-import { FC } from 'react'
+import { FC, useCallback, useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { numberWithCommas } from 'utils/number'
 import { HomeCards } from './HomeCards'
 
+type TelemetryObject = [string, string, number, number]
+type TelemetryData = TelemetryObject[]
+
 export const HomeChainInfo: FC = () => {
   const { ref, inView } = useInView()
   const { loading, data } = useHomeCardsQueryQuery({ skip: !inView })
+  const [rawNodeCount, setRawNodeCount] = useState<number>(0)
+
+  const getTelemetryData = useCallback(async () => {
+    const ws = new WebSocket('wss://telemetry.subspace.network/feed')
+    ws.onopen = () => {
+      console.log('Telemetry WebSocket connected')
+      ws.send(JSON.stringify({ ping: 1 }))
+    }
+
+    ws.onmessage = async (event) => {
+      try {
+        const text = await event.data.text()
+        const jsonData: TelemetryData = JSON.parse(text)
+        const filterNetwork = jsonData.filter(
+          (item: TelemetryObject) => item[0] === 'Autonomys Mainnet',
+        )
+        const nodeCount = filterNetwork.reduce(
+          (max: number, current: TelemetryObject) => (current[2] > max ? current[2] : max),
+          filterNetwork[0][2],
+        )
+        setRawNodeCount(nodeCount)
+      } catch (err) {
+        console.error('Failed to parse Blob as JSON:', err)
+      }
+    }
+
+    ws.onerror = (error) => console.error('Telemetry WebSocket error:', error)
+    ws.onclose = () => console.log('WebSocket disconnected')
+
+    return () => ws.close()
+  }, [])
 
   const spacePledgedVal = data ? Number(data.consensus_blocks[0].space_pledged) : 0
   const spacePledged = formatSpaceToDecimal(spacePledgedVal)
-  const spacePledgedBinary = formatSpaceToBinary(spacePledgedVal)
   const historySizeVal = data ? Number(data.consensus_blocks[0].blockchain_size) : 0
   const historySize = formatSpaceToDecimal(historySizeVal)
-  const historySizeBinary = formatSpaceToBinary(historySizeVal)
   const blocksCount = data ? numberWithCommas(Number(data.consensus_blocks[0].height)) : 'error'
+  const nodeCount = numberWithCommas(rawNodeCount)
+
+  useEffect(() => {
+    getTelemetryData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div ref={ref}>
@@ -26,9 +64,8 @@ export const HomeChainInfo: FC = () => {
         <HomeCards
           blocksCount={blocksCount}
           spacePledged={spacePledged}
-          spacePledgedBinary={spacePledgedBinary}
+          nodeCount={nodeCount}
           historySize={historySize}
-          historySizeBinary={historySizeBinary}
         />
       )}
     </div>
