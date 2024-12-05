b0dfc69c463df00be5660061e391f8f0ca7c2ccc
Marc-Aurele Besner <marcaurele.desk@gmail.com>
2024-12-05 09:59:30 -0500

    improve the route protection and improve metadata generation

--- explorer/src/app/[chain]/leaderboard/nominators/page.tsx
+++ explorer/src/app/[chain]/leaderboard/nominators/page.tsx
@@ -1,31 +1,25 @@
+import { getMetadata } from '@/utils/metadata/basic'
 import { NotFound } from 'components/layout/NotFound'
 import { NominatorLeaderboard } from 'components/Leaderboard/index'
-import { indexers } from 'constants/indexers'
-import { metadata } from 'constants/metadata'
-import { Routes } from 'constants/routes'
+import { Routes, RoutesLeaderboard } from 'constants/routes'
 import { Metadata } from 'next'
 import { FC } from 'react'
 import type { ChainPageProps } from 'types/app'
 import { isRouteSupportingNetwork } from 'utils/route'
 
-export async function generateMetadata({ params: { chain } }: ChainPageProps): Promise<Metadata> {
-  const chainTitle = indexers.find((c) => c.network === chain)?.title || 'Unknown chain'
-  const title = `${metadata.title} - ${chainTitle} - Nominator Leaderboard`
-  return {
-    ...metadata,
-    title,
-    openGraph: {
-      ...metadata.openGraph,
-      title,
-    },
-    twitter: {
-      ...metadata.twitter,
-      title,
-    },
-  }
-}
+export const generateMetadata = ({ params: { chain } }: ChainPageProps): Metadata =>
+  getMetadata(
+    chain,
+    'Top Nominators',
+    undefined,
+    `${chain}/${Routes.leaderboard}/${RoutesLeaderboard.nominators}`,
+  )
 
 const Page: FC<ChainPageProps> = ({ params: { chain } }) =>
-  isRouteSupportingNetwork(chain, Routes.leaderboard) ? <NominatorLeaderboard /> : <NotFound />
+  isRouteSupportingNetwork(chain, Routes.leaderboard, RoutesLeaderboard.nominators) ? (
+    <NominatorLeaderboard />
+  ) : (
+    <NotFound />
+  )
 
 export default Page
--- explorer/src/app/[chain]/leaderboard/page.tsx
+++ explorer/src/app/[chain]/leaderboard/page.tsx
@@ -1,11 +1,19 @@
-'use client'
-
+import { getMetadata } from '@/utils/metadata/basic'
 import { NotFound } from 'components/layout/NotFound'
 import { FarmerLeaderboard } from 'components/Leaderboard'
-import { Routes } from 'constants/routes'
+import { Routes, RoutesLeaderboard } from 'constants/routes'
+import { Metadata } from 'next'
 import { FC } from 'react'
 import type { ChainPageProps } from 'types/app'
 import { isRouteSupportingNetwork } from 'utils/route'
+
+export const generateMetadata = ({ params: { chain } }: ChainPageProps): Metadata =>
+  getMetadata(
+    chain,
+    'Top Farmers',
+    undefined,
+    `${chain}/${Routes.leaderboard}/${RoutesLeaderboard.farmers}`,
+  )
 
 const Page: FC<ChainPageProps> = ({ params: { chain } }) =>
   isRouteSupportingNetwork(chain, Routes.leaderboard) ? <FarmerLeaderboard /> : <NotFound />
