879d9acdb6301b52693b31a259461a78aca785ba
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-01-06 10:05:17 -0500

    customize swagger

--- indexers/api/src/main.ts
+++ indexers/api/src/main.ts
@@ -7,14 +7,67 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Your API')
-    .setDescription('API documentation')
-    .setVersion('1.0')
+    .setTitle('Autonomys Astral API')
+    .setDescription('Autonomys Astral API documentation')
+    .setVersion('0.0.1')
     .addApiKey({ type: 'apiKey', name: 'X-API-KEY', in: 'header' }, 'X-API-KEY')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  const customCss = `
+    /* Logo and TopBar Styling */
+    .swagger-ui .topbar { 
+      background-color: #27355D;
+      padding: 10px 0;
+    }
+
+    /* Method Badges */
+    .swagger-ui .opblock-get .opblock-summary-method { 
+      background-color: #ABCFEF; 
+    }
+    .swagger-ui .opblock-post .opblock-summary-method { 
+      background-color: #91D3A0; 
+    }
+    .swagger-ui .opblock-put .opblock-summary-method { 
+      background-color: #fca130; 
+    }
+    .swagger-ui .opblock-delete .opblock-summary-method { 
+      background-color: #f93e3e; 
+    }
+
+    /* Improve Contrast and Readability */
+    .swagger-ui .opblock-description-wrapper p,
+    .swagger-ui .opblock-external-docs-wrapper p,
+    .swagger-ui .opblock-title_normal p {
+      color: #3b4151;
+    }
+
+    /* Make the API version number more visible */
+    .swagger-ui .info .title small {
+      background: #27355D;
+      color: white;
+    }
+
+    /* Optional: Add a subtle hover effect to tryout buttons */
+    .swagger-ui .btn.try-out__btn:hover {
+      background-color: #344268;
+    }
+  `;
+
+  SwaggerModule.setup('api', app, document, {
+    customCss,
+    customfavIcon: '/assets/favicon.ico',
+    customSiteTitle: 'Autonomys Astral API Documentation',
+    swaggerOptions: {
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+      displayRequestDuration: true,
+      defaultModelsExpandDepth: 3,
+      defaultModelExpandDepth: 3,
+    },
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
