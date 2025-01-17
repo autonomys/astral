b461c38c8596203d509055cb3cf2979c5a01852d
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-01-17 15:29:44 -0500

    add multiple mapping optimization

--- indexers/consensus/src/mappings/utils.ts
+++ indexers/consensus/src/mappings/utils.ts
@@ -1,24 +1,4 @@
 import { capitalizeFirstLetter } from "@autonomys/auto-utils";
-import { SubstrateBlock } from "@subql/types";
-
-export const dateEntry = (blockNumber: number | bigint) => {
-  if (typeof blockNumber === "number") {
-    blockNumber = BigInt(blockNumber);
-  }
-  return {
-    createdAt: blockNumber,
-    updatedAt: blockNumber,
-  };
-};
-
-export const getBlockNumberFromBlock = (block: SubstrateBlock): number => {
-  try {
-    return block.block.header.number.toNumber();
-  } catch (error) {
-    logger.error(`Error getting block number: ${error}`);
-    throw error;
-  }
-};
 
 export const decodeLog = (value: null | Uint8Array | Uint8Array[]) => {
   if (!value) return null;
