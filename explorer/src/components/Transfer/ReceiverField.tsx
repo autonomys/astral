8728f3d9102ae1af74b52fd4884b565f08fb78f4
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-02-21 13:14:08 -0500

    add address book selector in receiver field

--- explorer/src/components/Transfer/ReceiverField.tsx
+++ explorer/src/components/Transfer/ReceiverField.tsx
@@ -1,6 +1,12 @@
-import { Field, Form, Formik } from 'formik'
+import { shortString } from '@autonomys/auto-utils'
+import { Listbox, Transition } from '@headlessui/react'
+import { WalletType } from 'constants/wallet'
+import { Field, FieldArray, Form, Formik } from 'formik'
+import useWallet from 'hooks/useWallet'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { FC, useCallback, useMemo } from 'react'
+import { FC, Fragment, useCallback, useMemo, useState } from 'react'
+import { useAddressBookStates } from 'states/addressBook'
+import { formatAddress } from 'utils/formatAddress'
 import * as Yup from 'yup'
 
 interface FormValues {
@@ -8,9 +14,12 @@ interface FormValues {
 }
 
 export const ReceiverField: FC = () => {
+  const { actingAccount, accounts } = useWallet()
   const router = useRouter()
   const pathname = usePathname()
   const searchParams = useSearchParams()
+  const [addressBookIsOpen, setAddressBookIsOpen] = useState<boolean>(false)
+  const { addresses } = useAddressBookStates()
 
   const receiver = searchParams.get('receiver')
 
@@ -43,21 +52,117 @@ export const ReceiverField: FC = () => {
           enableReinitialize
           onSubmit={() => {}}
         >
-          {({ errors, touched, handleSubmit }) => (
+          {({ errors, touched, handleSubmit, setFieldValue }) => (
             <Form className='w-full' onSubmit={handleSubmit} data-testid='testOperatorStakeForm'>
               <div className='flex items-center'>
-                <Field
-                  name='address'
-                  placeholder=''
-                  type='text'
-                  onChange={handleSetReceiver}
-                  className={`mt-4 block w-full rounded-full bg-white from-primaryAccent to-blueUndertone px-4 py-[10px] text-sm text-gray-900 shadow-lg dark:bg-gradient-to-r dark:text-white
-                    ${
-                      errors.address &&
-                      touched.address &&
-                      'block w-full rounded-full bg-white px-4 py-[10px] text-sm text-gray-900 shadow-lg dark:bg-blueDarkAccent'
-                    }
-                  `}
+                <FieldArray
+                  name='dischargeNorms'
+                  render={() => (
+                    <div className='relative w-full'>
+                      <Field
+                        name='address'
+                        placeholder=''
+                        type='text'
+                        onChange={handleSetReceiver}
+                        className={`mt-4 block w-full rounded-full bg-white from-primaryAccent to-blueUndertone px-4 py-[10px] text-sm text-gray-900 shadow-lg dark:bg-gradient-to-r dark:text-white
+                        ${
+                          errors.address &&
+                          touched.address &&
+                          'block w-full rounded-full bg-white px-4 py-[10px] text-sm text-gray-900 shadow-lg dark:bg-blueDarkAccent'
+                        }
+                      `}
+                      />
+
+                      <Listbox value={actingAccount}>
+                        <Listbox.Button
+                          className={
+                            'absolute mt-2 flex items-center gap-2 rounded-full bg-grayDarker px-2 text-sm font-medium text-white dark:bg-primaryAccent md:space-x-4 md:text-base'
+                          }
+                          style={{ right: '10px', top: '50%', transform: 'translateY(-50%)' }}
+                          onClick={() => setAddressBookIsOpen(!addressBookIsOpen)}
+                        >
+                          Address book
+                        </Listbox.Button>
+                        <Transition
+                          as={Fragment}
+                          leave='transition ease-in duration-100'
+                          leaveFrom='opacity-100'
+                          leaveTo='opacity-0'
+                        >
+                          <Listbox.Options className='absolute right-0 z-50 mt-1 max-h-40 w-auto overflow-auto rounded-md bg-white py-2 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-blueAccent dark:text-white sm:text-sm md:w-full'>
+                            {accounts &&
+                              accounts.map((account, chainIdx) => {
+                                const subAccount =
+                                  account.type === WalletType.subspace ||
+                                  (account as { type: string }).type === 'sr25519'
+                                    ? formatAddress(account.address)
+                                    : account.address
+                                const formattedAccount = subAccount && shortString(subAccount)
+                                return (
+                                  <Listbox.Option
+                                    key={chainIdx}
+                                    className={({ active }) =>
+                                      `relative z-50 cursor-default select-none py-2 pr-4 text-gray-900 dark:text-white ${
+                                        active && 'bg-gray-100 dark:bg-blueDarkAccent'
+                                      }`
+                                    }
+                                    value={account}
+                                    onClick={() => setFieldValue('address', subAccount)}
+                                  >
+                                    {({ selected }) => {
+                                      return (
+                                        <div className='px-2'>
+                                          <span
+                                            className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}
+                                          >
+                                            {account.name} {formattedAccount}{' '}
+                                            <span className='my-0 ml-4 rounded-full bg-grayDarker px-1 text-xs font-medium text-white dark:bg-primaryAccent md:space-x-6 md:text-xs'>
+                                              Wallet
+                                            </span>
+                                          </span>
+                                        </div>
+                                      )
+                                    }}
+                                  </Listbox.Option>
+                                )
+                              })}
+                            {addresses &&
+                              addresses.map((address, index) => (
+                                <Listbox.Option
+                                  key={`address-book-saved-${index}-label-${address.label}`}
+                                  className={({ active }) =>
+                                    `relative z-50 cursor-default select-none py-2 pr-4 text-gray-900 dark:text-white ${
+                                      active && 'bg-gray-100 dark:bg-blueDarkAccent'
+                                    }`
+                                  }
+                                  value={address.address}
+                                  onClick={() => setFieldValue('address', address.address)}
+                                >
+                                  {({ selected }) => {
+                                    const subAccount = !address.address.startsWith('st')
+                                      ? formatAddress(address.address)
+                                      : address.address
+                                    const formattedAccount = subAccount && shortString(subAccount)
+                                    return (
+                                      <div className='px-2'>
+                                        <span
+                                          className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}
+                                        >
+                                          {address.label} {formattedAccount}{' '}
+                                          <span className='ml-4 rounded-full bg-grayDarker px-2 text-xs font-medium text-white dark:bg-primaryAccent md:space-x-6 md:text-xs'>
+                                            Saved
+                                          </span>
+                                        </span>
+                                      </div>
+                                    )
+                                  }}
+                                </Listbox.Option>
+                              ))}
+                          </Listbox.Options>
+                        </Transition>
+                      </Listbox>
+                    </div>
+                  )}
                 />
               </div>
               {errors.address && touched.address ? (
