f8de8254792aa9b71872f1c180bf97b1a0a75510
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-03-05 10:24:35 -0500

    improve dark theme on my nominations

--- explorer/src/components/Staking/NominationsTable.tsx
+++ explorer/src/components/Staking/NominationsTable.tsx
@@ -180,7 +180,7 @@ export const NominationsTable: FC = () => {
     <div className='flex w-full flex-col gap-5 px-4 align-middle'>
       {!subspaceAccount ? (
         <div className='flex flex-col items-center justify-center gap-4'>
-          <span className='py-4'>
+          <span className='py-4 dark:text-white'>
             Please connect a substrate-compatible wallet to view your nominations.
           </span>
           <WalletButton />
@@ -352,9 +352,9 @@ export const NominationsTable: FC = () => {
                   ))
                 ) : (
                   <div className='flex flex-col items-center justify-center gap-4'>
-                    <span className='py-4'>You have no nominations.</span>
+                    <span className='py-4 dark:text-white'>You have no nominations.</span>
                     <Link href={`/${network}/${Routes.staking}`}>
-                      <button className='relative w-full cursor-default rounded-full bg-primaryAccent from-primaryAccent to-blueUndertone py-[10px] pl-3 pr-16 text-left font-["Montserrat"] text-white shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 dark:bg-gradient-to-r dark:text-white sm:text-sm md:pr-10'>
+                      <button className='relative w-full cursor-pointer rounded-full bg-primaryAccent from-primaryAccent to-blueUndertone py-[10px] pl-3 pr-16 text-left font-["Montserrat"] text-white shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 dark:bg-gradient-to-r dark:text-white sm:text-sm md:pr-10'>
                         <div className='flex items-center justify-center'>
                           <span className='ml-2 w-28 text-center text-sm'>Operator list</span>
                         </div>
