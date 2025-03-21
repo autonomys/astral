cf70e2b68703c44b10d7182be43544ae73f3ac7c
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-03-21 09:35:58 -0400

    remove unneeded global prefix

--- indexers/api/src/main.ts
+++ indexers/api/src/main.ts
@@ -6,8 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
-
   app.enableCors({
     origin: true, // or specify origins like ['http://localhost:3000']
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
