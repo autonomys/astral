d282ec1dd9c6a7d1f4a6baf6cbaf54073329a069
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-04-02 09:35:35 -0400

    implement user-session and remove fauna

--- explorer/src/utils/auth/providers/subspace.ts
+++ explorer/src/utils/auth/providers/subspace.ts
@@ -3,11 +3,12 @@ import { AuthProvider, DEFAULT_DISCORD_TOKEN } from 'constants/session'
 import { User } from 'next-auth'
 import type { Provider } from 'next-auth/providers'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { findUserByID, saveUser, updateUser } from 'utils/fauna'
+import { findUserByID, saveUser } from '../user'
 import {
   verifySubspaceMainnetAccountRoles,
   verifySubspaceTaurusAccountRoles,
 } from '../vcs/subspace'
+import { verifyToken } from '../verifyToken'
 
 export const Subspace = () => {
   return CredentialsProvider({
@@ -36,6 +37,7 @@ export const Subspace = () => {
         // Return null if the credentials are invalid
         if (!isValid) return null
 
+        const session = verifyToken()
         const did = `did:subspace:${account}`
 
         // Verify Subspace VCs for mainnet
@@ -57,12 +59,12 @@ export const Subspace = () => {
         // create the user object if the credentials are valid
         const user: User = {
           id: did,
-          DIDs: [did],
+          DIDs: [...(savedUser?.data?.DIDs || []), did],
           subspace: {
+            ...(savedUser?.data?.subspace ?? {}),
             account,
-            message,
-            signature,
             vcs: {
+              ...(savedUser?.data?.subspace?.vcs ?? {}),
               mainnetFarmer,
               mainnetOperator,
               mainnetNominator,
@@ -71,24 +73,33 @@ export const Subspace = () => {
               taurusNominator,
             },
           },
-          discord: DEFAULT_DISCORD_TOKEN,
+          discord: {
+            ...(savedUser?.data?.discord ?? DEFAULT_DISCORD_TOKEN),
+            ...session.discord,
+          },
         }
 
-        if (!savedUser || savedUser.length === 0) {
+        if (!savedUser) {
           console.log('User does not exist, saving user:', user)
-          await saveUser(user)
-
+          await saveUser(user.id, {
+            ...user,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          })
           return user
+        } else {
+          console.log('User exists, updating user:', user)
+          await saveUser(user.id, {
+            ...savedUser.data,
+            ...user,
+            [AuthProvider.subspace]: user[AuthProvider.subspace],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          })
         }
-        await updateUser(
-          savedUser[0].ref,
-          savedUser[0].data,
-          AuthProvider.subspace,
-          user.subspace ?? {},
-        )
 
         return {
-          ...savedUser[0].data,
+          ...savedUser.data,
           [AuthProvider.subspace]: user[AuthProvider.subspace],
         }
       } catch (error) {
