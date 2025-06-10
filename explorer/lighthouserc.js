f1155019255b66132d09d6d2dc745f12223a4bf8
Naman Anand <iamnamananand996@gmail.com>
2025-06-10 11:12:10 +0530

    fix: path update

--- explorer/lighthouserc.js
+++ explorer/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'yarn start',
+      startServerCommand: 'cd explorer && yarn start',
       url: ['http://localhost:3000'],
       numberOfRuns: 3,
     },
