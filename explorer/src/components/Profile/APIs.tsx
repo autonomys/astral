67a9984a965c9322f58b6ccb8d772834e58ba72e
Naman Anand <iamnamananand996@gmail.com>
2025-06-19 23:21:40 +0530

    chore: update larger component to lazy component

--- explorer/src/components/Profile/APIs.tsx
+++ explorer/src/components/Profile/APIs.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import dynamic from 'next/dynamic'
 import { useParams } from 'next/navigation'
 import { FC, useEffect, useState } from 'react'
-import SwaggerUI from 'swagger-ui-react'
 import 'swagger-ui-react/swagger-ui.css'
+
+const DynamicSwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false })
 
 export const APIs: FC = () => {
   const { chain } = useParams()
@@ -67,7 +69,7 @@ export const APIs: FC = () => {
             <div className='border-primary h-12 w-12 animate-spin rounded-full border-b-2 border-t-2'></div>
           </div>
         ) : (
-          <SwaggerUI spec={specData} />
+          <DynamicSwaggerUI spec={specData} />
         )}
       </div>
     </div>
