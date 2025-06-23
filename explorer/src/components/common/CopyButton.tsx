f59b176dcb2a4d1184716ee22485b4d594459206
Naman Anand <iamnamananand996@gmail.com>
2025-06-23 19:24:32 +0530

    refactor: 🔨 copy button

--- explorer/src/components/common/CopyButton.tsx
+++ explorer/src/components/common/CopyButton.tsx
@@ -1,7 +1,6 @@
-import { Square2StackIcon } from '@heroicons/react/24/outline'
+import { CheckIcon, Square2StackIcon } from '@heroicons/react/24/outline'
 import { useClipboard } from 'hooks/useClipboard'
-import { ButtonHTMLAttributes, FC, ReactNode } from 'react'
-import toast from 'react-hot-toast'
+import { ButtonHTMLAttributes, FC, ReactNode, useState } from 'react'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   value: string
@@ -19,19 +18,50 @@ export const CopyButton: FC<Props> = ({
   iconClass = 'w-4 h-4',
   ...rest
 }) => {
-  const { onCopy } = useClipboard(value)
+  const { onCopy, hasCopied } = useClipboard(value)
+  const [showTooltip, setShowTooltip] = useState(false)
 
   const handleCopyClick = (): void => {
     onCopy()
-    toast.success(message, {
-      position: 'bottom-center',
-    })
   }
 
+  const handleMouseEnter = (): void => {
+    setShowTooltip(true)
+  }
+
+  const handleMouseLeave = (): void => {
+    setShowTooltip(false)
+  }
+
+  const tooltipMessage = hasCopied ? message : 'Copy to clipboard'
+
   return (
-    <button className={`flex gap-2 ${buttonClass}`} onClick={handleCopyClick} {...rest}>
-      {children}
-      <Square2StackIcon className={iconClass} stroke='#1949D2' />
-    </button>
+    <div className='relative inline-block'>
+      <button
+        className={`relative flex gap-2 ${buttonClass}`}
+        onClick={handleCopyClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        {...rest}
+      >
+        {children}
+        {hasCopied ? (
+          <CheckIcon className={`${iconClass} my-auto stroke-green-500`} />
+        ) : (
+          <Square2StackIcon
+            className={`${iconClass} my-auto stroke-blue-500 hover:stroke-blue-600`}
+          />
+        )}
+      </button>
+
+      {/* Tooltip */}
+      {showTooltip && (
+        <div className='absolute bottom-full right-[-10px] z-50 mb-2 whitespace-nowrap rounded-lg bg-gray-900 px-3 py-1.5 text-xs text-white shadow-lg'>
+          {tooltipMessage}
+          {/* Tooltip arrow */}
+          <div className='absolute right-4 top-full h-0 w-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900'></div>
+        </div>
+      )}
+    </div>
   )
 }
