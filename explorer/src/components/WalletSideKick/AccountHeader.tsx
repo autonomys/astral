684e3e559ac1ac0dabe9b31f664ff492569ffa51
Naman Anand <iamnamananand996@gmail.com>
2025-06-23 21:00:52 +0530

    refactor: 🔨 tooltip

--- explorer/src/components/WalletSideKick/AccountHeader.tsx
+++ explorer/src/components/WalletSideKick/AccountHeader.tsx
@@ -1,5 +1,4 @@
 import { CopyButton } from 'components/common/CopyButton'
-import { Tooltip } from 'components/common/Tooltip'
 import useWallet from 'hooks/useWallet'
 import { FC } from 'react'
 import { limitNumberDecimals } from 'utils/number'
@@ -26,12 +25,10 @@ export const AccountHeader: FC<AccountHeaderProps> = ({ walletBalance, tokenSymb
           className='block w-[200px] rounded-lg border-primaryAccent bg-transparent px-4 text-sm text-gray-900 shadow-lg dark:bg-blueAccent dark:text-white'
         />
         <div className='ml-2'>
-          <Tooltip text='Copy wallet address'>
-            <CopyButton
-              value={subspaceAccount ?? actingAccount.address}
-              message='Wallet address copied'
-            />
-          </Tooltip>
+          <CopyButton
+            value={subspaceAccount ?? actingAccount.address}
+            message='Wallet address copied'
+          />
         </div>
       </div>
       <div className='m-2 flex items-center justify-center'>
